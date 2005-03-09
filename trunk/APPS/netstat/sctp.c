630d0dbbb13231c47f893c576fed1931ca3a3d50
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2005-03-09 15:55:17 +0000

    syntax fix for SCTP.c in netstat... the ra->ro change that was done a while ago.

--- trunk/APPS/netstat/sctp.c
+++ trunk/APPS/netstat/sctp.c
@@ -366,7 +366,7 @@ sctp_protopr(off, name)
 					kread((u_long)nets_next, (char *)&nets,
 					    sizeof(nets));
 					nets_next = nets.sctp_next.tqe_next;
-					sa = (struct sockaddr *)&nets.ra._l_addr;
+					sa = (struct sockaddr *)&nets.ro._l_addr;
 					if (sa->sa_family == AF_INET) {
 						sin_r = (struct sockaddr_in *)sa;
 					} else if (sa->sa_family == AF_INET6) {
