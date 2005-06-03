0fbbb72fab89dc4afcb26cf76cdb067e8b51f720
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2005-06-03 19:08:29 +0000

    - ip6_output takes one more argument (lock). I think 0 is the correct value. - Not all sbunlock() where replaced by the MACROS.

--- KERN/netinet/sctp_output.c
+++ KERN/netinet/sctp_output.c
@@ -60,6 +60,10 @@
 #include <sys/sysctl.h>
 #include <sys/resourcevar.h>
 #include <sys/uio.h>
+#if defined(__APPLE__) && !defined(SCTP_APPLE_PANTHER)
+#include <sys/proc_internal.h>
+#include <sys/uio_internal.h>
+#endif
 #ifdef INET6
 #include <sys/domain.h>
 #endif
@@ -2530,6 +2534,9 @@ sctp_lowlevel_chunk_output(struct sctp_inpcb *inp,
 #if (defined(__FreeBSD__) && __FreeBSD_version >= 480000)
 		    , NULL
 #endif
+#if defined(__APPLE__) && !defined(SCTP_APPLE_PANTHER)
+		    , 0
+#endif
 			);
 		if (net) {
 			/* for link local this must be done */
@@ -4380,7 +4387,7 @@ sctp_msg_append(struct sctp_tcb *stcb,
 			 * drop to spl0() so that others can
 			 * get in.
 			 */
-			sbunlock(&so->so_snd);
+			SOCKBUF_UNLOCK(&so->so_snd);
 			be.error = 0;
 			stcb->block_entry = &be;
 			SCTP_TCB_UNLOCK(stcb);
@@ -4742,7 +4749,7 @@ zap_by_it_all:
 #endif
 
 release:
-	sbunlock(&so->so_snd);
+	SOCKBUF_UNLOCK(&so->so_snd);
 out_locked:
 	SOCKBUF_UNLOCK(&so->so_snd);
 out:
@@ -8398,6 +8405,9 @@ sctp_send_shutdown_complete2(struct mbuf *m, int iphlen, struct sctphdr *sh)
 #if (defined(__FreeBSD__) && __FreeBSD_version >= 480000)
 			   , NULL
 #endif
+#if defined(__APPLE__) && !defined(SCTP_APPLE_PANTHER)
+			   , 0
+#endif
 		    );
 		/* Free the route if we got one back */
 		if (ro.ro_rt)
@@ -9314,6 +9324,9 @@ sctp_send_abort(struct mbuf *m, int iphlen, struct sctphdr *sh, uint32_t vtag,
 #if (defined(__FreeBSD__) && __FreeBSD_version >= 480000)
 		    , NULL
 #endif
+#if defined(__APPLE__) && !defined(SCTP_APPLE_PANTHER)
+		    , 0
+#endif
 		    );
 		/* Free the route if we got one back */
 		if (ro.ro_rt)
@@ -9451,6 +9464,9 @@ sctp_send_operr_to(struct mbuf *m, int iphlen,
 #endif
 #if (defined(__FreeBSD__) && __FreeBSD_version >= 480000)
 	    , NULL
+#endif
+#if defined(__APPLE__) && !defined(SCTP_APPLE_PANTHER)
+		    , 0
 #endif
 		);
 		sctp_pegs[SCTP_DATAGRAMS_SENT]++;
@@ -9633,7 +9649,7 @@ sctp_copy_it_in(struct sctp_inpcb *inp,
 			sctp_log_block(SCTP_BLOCK_LOG_INTO_BLK,
 			    so, asoc);
 #endif
-			sbunlock(&so->so_snd);
+			SOCKBUF_UNLOCK(&so->so_snd);
 			be.error = 0;
 			stcb->block_entry = &be;
 			SCTP_TCB_UNLOCK(stcb);
@@ -9751,7 +9767,6 @@ sctp_copy_it_in(struct sctp_inpcb *inp,
 					mm = NULL;
 				}
 			}
-			sbunlock(&so->so_snd);
 			SOCKBUF_UNLOCK(&so->so_snd);
 			sctp_abort_an_association(stcb->sctp_ep, stcb,
 						  SCTP_RESPONSE_TO_USER_REQ,
@@ -10135,7 +10150,7 @@ zap_by_it_now:
 #endif
 
 release:
-	sbunlock(&so->so_snd);
+	SOCKBUF_UNLOCK(&so->so_snd);
 out_locked:
 	SOCKBUF_UNLOCK(&so->so_snd);
 out_notlocked:
@@ -10461,7 +10476,7 @@ sctp_sosend(struct socket *so,
 #if defined(__FreeBSD__) && __FreeBSD_version >= 500000
 		p->td_proc->p_stats->p_ru.ru_msgsnd++;
 #else
-	p->p_stats->p_ru.ru_msgsnd++;
+		p->p_stats->p_ru.ru_msgsnd++;
 #endif
 
 	if (stcb) {
