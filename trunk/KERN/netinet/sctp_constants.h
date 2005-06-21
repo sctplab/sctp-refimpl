a540c2dc2f25026bb3d7da653021368640d09c16
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2005-06-21 13:44:07 +0000

    adds a cwnd report for early fr

--- trunk/KERN/netinet/sctp_constants.h
+++ trunk/KERN/netinet/sctp_constants.h
@@ -99,6 +99,7 @@
 #define SCTP_LOG_SBRESULT           55
 #define SCTP_FR_DUPED               56
 #define SCTP_FR_MARKED_EARLY        57
+#define SCTP_FR_CWND_REPORT         58
 /*
  * To turn on various logging, you must first define SCTP_STAT_LOGGING.
  * Then to get something to log you define one of the logging defines i.e.
