7044ced7dc98cb804b9e624f3af9bc68a16703a6
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2005-07-15 15:47:03 +0000

    fix so we panic if the sent_queue is out of order (instead of throwing away the chunk). the alternative is to abort the assoc... throwing away the chunk will lead to a deadlock :-0
    I also now fix the putting on the sent_queue to
    monitor to validate that the order is correctly
    maintained.

--- trunk/KERN/netinet/sctp_timer.c
+++ trunk/KERN/netinet/sctp_timer.c
@@ -592,6 +592,8 @@ sctp_mark_all_for_resend(struct sctp_tcb *stcb,
 		    (stcb->asoc.last_acked_seq == chk->rec.data.TSN_seq)) {
 			/* Strange case our list got out of order? */
 			printf("Our list is out of order?\n");
+			panic("Out of order list");
+			/*
 			TAILQ_REMOVE(&stcb->asoc.sent_queue, chk, sctp_next);
 			if (chk->data) {
 				sctp_release_pr_sctp_chunk(stcb, chk, 0xffff,
@@ -605,6 +607,7 @@ sctp_mark_all_for_resend(struct sctp_tcb *stcb,
 			SCTP_DECR_CHK_COUNT();
 			SCTP_ZONE_FREE(sctppcbinfo.ipi_zone_chunk, chk);
 			continue;
+			*/
 		}
 		if ((chk->whoTo == net) && (chk->sent < SCTP_DATAGRAM_ACKED)) {
 			/* found one to mark:
