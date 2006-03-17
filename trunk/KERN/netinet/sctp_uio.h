167e68c2d364cb92a2b6b1556803fd228ade5d7b
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-03-17 10:44:01 +0000

    Fixes stdint.h include.. its NOT in kernel land.

--- trunk/KERN/netinet/sctp_uio.h
+++ trunk/KERN/netinet/sctp_uio.h
@@ -34,8 +34,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
+#if ! defined(_KERNEL)
 #include <stdint.h>
+#endif
 #include <sys/types.h>
 #include <sys/socket.h>
 
