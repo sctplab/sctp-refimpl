314271aeda7b81c62e45cbdb5a4fa427ba102286
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-04-06 14:52:12 +0000

    Adds trylock so we can see how many times the sender gets blocked on re-locking the TCB after releasing it before the data copy.

--- KERN/netinet/sctp_constants.h
+++ KERN/netinet/sctp_constants.h
@@ -844,7 +844,7 @@
 #define SCTP_AUTH_RCVD_NULL_KEY  127
 #define SCTP_NOSEND_NET_INPUT    128
 #define SCTP_SBWAIT_ON_SEND      129
-#define SCTP_WAKEUP_DEFERED      130
+#define SCTP_SND_WAIT_OLOCK      130
 #define SCTP_WAKEUP_CALLED       131
 /*
  * This value defines the number of vtag block time wait entry's
