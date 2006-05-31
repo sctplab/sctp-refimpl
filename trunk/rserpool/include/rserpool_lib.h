c42dbfd751cf974abacfcaf5668348c1d97634b0
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-05-31 13:34:57 +0000

    more updates

--- trunk/rserpool/include/rserpool_lib.h
+++ trunk/rserpool/include/rserpool_lib.h
@@ -27,20 +27,54 @@
  * This table determines which PE/PU we are.
  *
  * sd --sd2pool--> hashes out a hash table for the socket
+ * you get back a pointer to a struct xxx
  *
  */
 
+/* The extern socket hash */
 extern HashedTbl *sd_pool;
+/* do I need a mutex? */
 
-/* first the pool */
-struct rsp_pool {
-	char *name;
-	uint32_t name_len;
-	dlist_t *pe_list;
-	int sd; 
-	uint32_t refcount;
+struct rsp_socket_hash {
+	int	 	sd;			/* sctp socket */
+	HashedTbl	*cache;			/* cache of names */
+	HashedTbl	*vtagHash;		/* assoc id-> rsp_pool */
+	HashedTbl	*ipaddrPortHash		/* ipadd -> rsp_pool_element */
+	uint32_t 	refcnt;			/* number of names in use */
+	dlist_t 	*enrpAddrList;		/* Home ENRP server */
+	char 		*registeredName;	/* our name if registered */
+	uint16_t	port;			/* our port number */
+	uint8_t		registered;		/* boolean flag */
 };
 
+/* A pool entry */
+struct rsp_pool {
+	char 		*name;
+	uint32_t 	name_len;
+	dlist_t 	*peList;
+	uint32_t 	refcnt;
+	uint32_t	regType;
+	uint32_t	policy_value;
+	uint32_t	policy_actvalue;
+	uint8_t         auto_update;
+};
+
+/* Each entry aka the actual PE */
+struct rsp_pool_element {
+	char 		*name; 		/* pointer to pool name */
+	struct rsp_pool *pool;		/* pointer to pool entry */
+	dlist_t 	*addrList;	/* list of addresses */
+	sctp_assoc_t	asocid;		/* sctp asoc id */
+	int		sd;		/* sctp socket */
+};
+
+/* An address entry in the list */
+struct pe_address {
+	union {
+		struct sockaddr_in  sin;
+		struct sockaddr_in6 sin6;
+	}sa;
+};
 
 
 #endif
