293f0a12bbd44e3c4afaec585a217ff6a4dd15de
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-07-19 11:22:16 +0000

    beginning of handler of cookie and BC.

---
+++ trunk/rserpool/rsplib/rsp_asap.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <rserpool_lib.h>
+#include <rserpool.h>
+#include <rserpool_io.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#include <rserpool_util.h>
+#include <sys/errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
