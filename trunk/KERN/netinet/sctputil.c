55a4813da36dea0ce5ee8256eb206e76972dece5
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-08-09 18:59:03 +0000

    more syntax

--- trunk/KERN/netinet/sctputil.c
+++ trunk/KERN/netinet/sctputil.c
@@ -233,16 +233,16 @@ sctp_log_closing(struct sctp_inpcb *inp, struct sctp_tcb *stcb, int16_t loc)
 	sctp_clog[sctp_cwnd_log_at].time_event = sctp_get_time_of_event();
 	sctp_clog[sctp_cwnd_log_at].from = 0;
 	sctp_clog[sctp_cwnd_log_at].event_type = (uint8_t) SCTP_LOG_EVENT_CLOSE;
-	sctp_clog[sctp_cwnd_log_at].close.inp = (uint32_t)inp;
-	sctp_clog[sctp_cwnd_log_at].close.sctp_flags = inp->sctp_flags;
+	sctp_clog[sctp_cwnd_log_at].x.close.inp = (uint32_t)inp;
+	sctp_clog[sctp_cwnd_log_at].x.close.sctp_flags = inp->sctp_flags;
 	if(stcb) {
-		sctp_clog[sctp_cwnd_log_at].close.stcb = (uint32_t)stcb;
-		sctp_clog[sctp_cwnd_log_at].close.state = (uint16_t)stcb->sctp_ep->asoc.state;
+		sctp_clog[sctp_cwnd_log_at].x.close.stcb = (uint32_t)stcb;
+		sctp_clog[sctp_cwnd_log_at].x.close.state = (uint16_t)stcb->sctp_ep->asoc.state;
 	} else {
-		sctp_clog[sctp_cwnd_log_at].close.stcb = 0;
-		sctp_clog[sctp_cwnd_log_at].close.state = 0;
+		sctp_clog[sctp_cwnd_log_at].x.close.stcb = 0;
+		sctp_clog[sctp_cwnd_log_at].x.close.state = 0;
 	}
-	sctp_clog[sctp_cwnd_log_at].close.loc = loc;
+	sctp_clog[sctp_cwnd_log_at].close.x.loc = loc;
 	SCTP_STATLOG_UNLOCK();
 }
 
