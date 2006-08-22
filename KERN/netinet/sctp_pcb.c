7454163901a1925d82638d59a234f10e7e7863ab
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-08-22 19:56:22 +0000

    Ok two issues.
    In append_to_readqueue I was checking things BEFORE the
    lock. This is bad since that allowed the reader to change
    things on me ...causing a panic.
    
    Now for the peeloff/accept.. we DID have the right function in
    sctputil.c for moving the data to the new endpoint.. but it
    had the old locking..i.e. used the socketbuf lock.
    
    I have moved this to use the RIGHT lock.. but also there
    is the issue of how to prevent a reader from accessing the
    socket. So what we do is a sblock().. without wait. If it
    fails we unlock and give up. The reality is the ONLY one
    that will have this is someone doing a peeloff and
    a seperate thread doing a read() at the same time. And in
    such a case, we will leave the data stranded on the old
    socket... since to do otherwise would put the system in
    jeopardy.

--- KERN/netinet/sctp_pcb.c
+++ KERN/netinet/sctp_pcb.c
@@ -2055,31 +2055,6 @@ sctp_move_pcb_and_assoc(struct sctp_inpcb *old_inp, struct sctp_inpcb *new_inp,
 	    old_inp->sctp_ep.last_secret_number;
 	new_inp->sctp_ep.size_of_a_cookie = old_inp->sctp_ep.size_of_a_cookie;
 
-	/* fix up the socket buffer counts */
-	/* lock sockets */
-	if ((stcb->asoc.sb_cc) && (old_inp->sctp_flags & SCTP_PCB_FLAGS_TCPTYPE)) {
-		/*
-		 * stuff to read, move over the counts on the 1-2-1 type..
-		 * 1-2-M type do NOT need to do this since they do NOT
-		 * maintain the sb_cc and sb_mbcnt.
-		 */
-		SOCKBUF_LOCK(&(old_inp->sctp_socket)->so_rcv);
-		if ((old_inp->sctp_socket)->so_rcv.sb_cc >= stcb->asoc.sb_cc) {
-			(old_inp->sctp_socket)->so_rcv.sb_cc -= stcb->asoc.sb_cc;
-		} else {
-			(old_inp->sctp_socket)->so_rcv.sb_cc = 0;
-		}
-		if ((old_inp->sctp_socket)->so_rcv.sb_mbcnt >= stcb->asoc.sb_mbcnt) {
-			(old_inp->sctp_socket)->so_rcv.sb_mbcnt -= stcb->asoc.sb_mbcnt;
-		} else {
-			(old_inp->sctp_socket)->so_rcv.sb_mbcnt = 0;
-		}
-		SOCKBUF_UNLOCK(&(old_inp->sctp_socket)->so_rcv);
-		SOCKBUF_LOCK(&(new_inp->sctp_socket)->so_rcv);
-		(new_inp->sctp_socket)->so_rcv.sb_cc += stcb->asoc.sb_cc;
-		(new_inp->sctp_socket)->so_rcv.sb_mbcnt += stcb->asoc.sb_mbcnt;
-		SOCKBUF_UNLOCK(&(new_inp->sctp_socket)->so_rcv);
-	}
 	/* make it so new data pours into the new socket */
 	stcb->sctp_socket = new_inp->sctp_socket;
 	stcb->sctp_ep = new_inp;
