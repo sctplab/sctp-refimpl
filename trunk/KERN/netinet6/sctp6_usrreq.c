faffe98c5c735e9786512ae2a174ba6c5f350173
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-09-05 19:28:57 +0000

    We must have the same nulling and such in our pru_abort().

--- trunk/KERN/netinet6/sctp6_usrreq.c
+++ trunk/KERN/netinet6/sctp6_usrreq.c
@@ -800,9 +800,25 @@ sctp6_abort(struct socket *so)
 	if (((flags & SCTP_PCB_FLAGS_SOCKET_GONE) == 0) &&
 	    (atomic_cmpset_int(&inp->sctp_flags, flags, (flags | SCTP_PCB_FLAGS_SOCKET_GONE | SCTP_PCB_FLAGS_CLOSE_IP)))) {
 #ifdef SCTP_LOG_CLOSING
-		sctp_log_closing(inp, NULL, 17);
+		sctp_log_closing(inp, NULL, 16);
 #endif
 		sctp_inpcb_free(inp, 1, 0);
+		SOCK_LOCK(so);
+		so->so_snd.sb_cc = 0;
+		so->so_snd.sb_mb = NULL;
+		so->so_snd.sb_mbcnt = 0;
+		
+		/* same for the rcv ones, they are only
+		 * here for the accounting/select.
+		 */
+		so->so_rcv.sb_cc = 0;
+		so->so_rcv.sb_mb = NULL;
+		so->so_rcv.sb_mbcnt = 0;
+		/* Now null out the reference, we are
+		 * completely detached.
+		 */
+		so->so_pcb = NULL;
+		SOCK_UNLOCK(so);
 	} else {
 		flags = inp->sctp_flags;
 		if((flags & SCTP_PCB_FLAGS_SOCKET_GONE) == 0) {
