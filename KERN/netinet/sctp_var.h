24c5ce6548880a4e370472f0bf689a1b75074b50
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-09-16 08:13:19 +0000

    Get it compiling on Tiger. Not stable.

--- KERN/netinet/sctp_var.h
+++ KERN/netinet/sctp_var.h
@@ -585,10 +585,10 @@ extern int ticks;
 
 /* Apple KPI defines for atomic operations */
 #include <libkern/OSAtomic.h>
-#define atomic_add_int(addr, val)	OSAddAtomic(val, (SInt32 *)addr)
-#define atomic_subtract_int(addr, val)	OSAddAtomic((-val), (SInt32 *)addr)
-#define atomic_add_16(addr, val)	OSAddAtomic16(val, (SInt16 *)addr)
-
+#define atomic_add_int(addr, val)        OSAddAtomic(val, (SInt32 *)addr)
+#define atomic_subtract_int(addr, val)   OSAddAtomic((-val), (SInt32 *)addr)
+#define atomic_add_16(addr, val)         OSAddAtomic16(val, (SInt16 *)addr)
+#define atomic_cmpset_int(dst, exp, src) OSCompareAndSwap(exp, src, dst)
 /* additional protosw entries for Mac OS X 10.4 */
 #if defined(SCTP_APPLE_FINE_GRAINED_LOCKING)
 	int sctp_lock(struct socket *so, int refcount, int lr);
