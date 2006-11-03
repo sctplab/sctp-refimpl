8883596122994c1bf61a152587070fb97d41a8cf
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-11-03 20:33:53 +0000

    one more missed update and id sync.

--- KERN/netinet/sctp_asconf.c
+++ KERN/netinet/sctp_asconf.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_asconf.c,v 1.1 2006/11/03 15:23:15 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_asconf.c,v 1.2 2006/11/03 19:48:56 rrs Exp $");
 #endif
 
 #if !(defined(__OpenBSD__) || defined(__APPLE__))
