bbbba2205d5de2e4f07149829532325fefbe5bec
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-11-08 00:33:32 +0000

    Sync CVS to commited FreeBSD code. This also has a ifdef out code for Amita's bug on sending an INIT out on a peeled off assoc (we create a second asoc when we should not).

--- KERN/netinet/sctp_structs.h
+++ KERN/netinet/sctp_structs.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_structs.h,v 1.2 2006/11/05 13:25:17 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_structs.h,v 1.3 2006/11/08 00:21:13 rrs Exp $");
 #endif
 
 #ifndef __sctp_structs_h__
