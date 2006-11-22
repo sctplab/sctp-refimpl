12eda1e5cde98a29937dd0a5228265d3e71941a6
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-11-22 22:06:27 +0000

    Sync with main tree.

--- KERN/nke/sctp_nke/Tiger/netinet/sctp_input.c
+++ KERN/nke/sctp_nke/Tiger/netinet/sctp_input.c
@@ -4524,10 +4524,10 @@ sctp_common_input_processing(struct mbuf **mm, int iphlen, int offset,
 	 * Rest should be DATA only.  Check authentication state if AUTH for
 	 * DATA is required.
 	 */
-	if ((stcb != NULL) && !sctp_auth_disable &&
+	if ((length > offset) && (stcb != NULL) && !sctp_auth_disable &&
 	    sctp_auth_is_required_chunk(SCTP_DATA,
 	    stcb->asoc.local_auth_chunks) &&
-	    !stcb->asoc.authenticated && (length > offset)) {
+	    !stcb->asoc.authenticated) {
 		/* "silently" ignore */
 		SCTP_STAT_INCR(sctps_recvauthmissing);
 #ifdef SCTP_DEBUG
