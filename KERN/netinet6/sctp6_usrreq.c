e4c9106b21778299b68434943ba6e7c0edd76bb6
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-11-24 14:28:22 +0000

    commented out fix for shutdown-ack
    adds check for abort in a noport situation.

--- KERN/netinet6/sctp6_usrreq.c
+++ KERN/netinet6/sctp6_usrreq.c
@@ -358,7 +358,14 @@ sctp_skip_csum:
 			    (u_int8_t *) & chunk_buf);
 			sh->v_tag = init_chk->init.initiate_tag;
 		}
-		sctp_send_abort(m, iphlen, sh, 0, NULL);
+/*
+		if (ch->chunk_type == SCTP_SHUTDOWN_ACK) {
+			sctp_send_shutdown_complete2(m, iphlen, sh);
+			goto bad;
+		}
+*/
+		if(ch->chunk_type != SCTP_ABORT_ASSOCIATION)
+			sctp_send_abort(m, iphlen, sh, 0, NULL);
 		goto bad;
 	} else if (stcb == NULL) {
 		refcount_up = 1;
