49c943f2b0b974cc8c3c3da98d143b8379fd2afe
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-11-26 21:30:26 +0000

    Need to dance around this one too. Turns out we need to grab the readlock to re-check the GONE flag.

--- trunk/KERN/netinet/sctp_pcb.c
+++ trunk/KERN/netinet/sctp_pcb.c
@@ -4006,13 +4006,22 @@ sctp_free_assoc(struct sctp_inpcb *inp, struct sctp_tcb *stcb, int from_inpcbfre
 		 * hopefully given him something to chew on
 		 * above.
 		 */
-		if (so) {
-			/* Wake any reader/writers */
-			sctp_sorwakeup(inp, so);
-			sctp_sowwakeup(inp, so);
-		}
 		sctp_timer_start(SCTP_TIMER_TYPE_ASOCKILL, inp, stcb, NULL);
 		SCTP_TCB_UNLOCK(stcb);
+		if(so) {
+			SCTP_INP_RLOCK(inp);
+			if ((inp->sctp_flags & SCTP_PCB_FLAGS_SOCKET_ALLGONE) ||
+			    (inp->sctp_flags & SCTP_PCB_FLAGS_SOCKET_GONE)) 
+				/* nothing around */
+				so = NULL;
+			SCTP_INP_RUNLOCK(inp);
+			if (so) {
+				/* Wake any reader/writers */
+				sctp_sorwakeup(inp, so);
+				sctp_sowwakeup(inp, so);
+			}
+
+		}
 		splx(s);
 #ifdef SCTP_LOG_CLOSING
 		sctp_log_closing(inp, stcb, 9);
