a07574d7614d474f4648276cde45e8e028588cba
lei <lei@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2006-12-12 18:31:56 +0000

    split OpenBSD out of NetBSD os file move timer stuff together

--- trunk/KERN/netinet/sctp_os_openbsd.h
+++ trunk/KERN/netinet/sctp_os_openbsd.h
@@ -27,99 +27,58 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef __sctp_os_macosx_h__
-#define __sctp_os_macosx_h__
+#ifndef __sctp_os_openbsd_h__
+#define __sctp_os_openbsd_h__
 
 /*
  * includes
  */
-#include <sys/random.h>
-#include <netinet/sctp_callout.h>
+#include <dev/rndvar.h>
 
 /*
  * general memory allocation
  */
-#if defined(SCTP_APPLE_FINE_GRAINED_LOCKING)
-#define SCTP_MALLOC(var, type, size, name) \
-    do { \
-	MALLOC(var, type, size, M_PCB, M_WAITOK); \
-    } while (0)
-#else
 #define SCTP_MALLOC(var, type, size, name) \
     do { \
 	MALLOC(var, type, size, M_PCB, M_NOWAIT); \
     } while (0)
-#endif
 
 #define SCTP_FREE(var)	FREE(var, M_PCB)
 
-#define SCTP_MALLOC_SONAME(var, type, size) \
-    do { \
-	MALLOC(var, type, size, M_SONAME, M_WAITOK | M_ZERO); \
-    } while (0)
-#define SCTP_FREE_SONAME(var)	FREE(var, M_SONAME)
 
 /*
  * zone allocation functions
  */
-typedef struct vm_zone *sctp_zone_t;
-extern zone_t kalloc_zone(vm_size_t);	/* XXX */
+#include <sys/pool.h>
+typedef struct pool sctp_zone_t;
+void *sctp_pool_get(struct pool *, int);
+void sctp_pool_put(struct pool *, void *);
 
 /* SCTP_ZONE_INIT: initialize the zone */
 #define SCTP_ZONE_INIT(zone, name, size, number) \
-	zone = (void *)kalloc_zone(size);
+	pool_init(&(zone), size, 0, 0, 0, name, NULL);
 
 /* SCTP_ZONE_GET: allocate element from the zone */
 #define SCTP_ZONE_GET(zone) \
-	zalloc(zone);
+	sctp_pool_get(&zone, PR_NOWAIT);
 
 /* SCTP_ZONE_FREE: free element from the zone */
 #define SCTP_ZONE_FREE(zone, element) \
-	zfree(zone, element);
+	sctp_pool_put(&zone, element);
+
+/*
+ * timers
+ */
+#include <sys/timeout.h>
+typedef struct timeout sctp_os_timer_t;
 
 /*
  * Functions
  */
-#define sctp_read_random(buf, len)	read_random(buf, len)
-
-/* Mbuf access functions  */
-
-#define sctp_buf_len(m) (m->m_len)
-#define sctp_buf_next(m) (m->m_next)
-#define sctp_buf_hdr_len(m) (m->m_pkthdr.len)
-
+#define sctp_read_random(buf, len)	get_random_bytes(buf, len)
 
 /*
- * Other MacOS specific
+ * Other OpenBSD Specific
  */
-
-/* Apple KPI defines for atomic operations */
-#include <libkern/OSAtomic.h>
-#define atomic_add_int(addr, val)        OSAddAtomic(val, (SInt32 *)addr)
-#define atomic_subtract_int(addr, val)   OSAddAtomic((-val), (SInt32 *)addr)
-#define atomic_add_16(addr, val)         OSAddAtomic16(val, (SInt16 *)addr)
-#define atomic_cmpset_int(dst, exp, src) OSCompareAndSwap(exp, src, (UInt32 *)dst)
-
-/* additional protosw entries for Mac OS X 10.4 */
-#if defined(SCTP_APPLE_FINE_GRAINED_LOCKING)
-int sctp_lock(struct socket *so, int refcount, int lr);
-int sctp_unlock(struct socket *so, int refcount, int lr);
-
-#ifdef _KERN_LOCKS_H_
-lck_mtx_t *sctp_getlock(struct socket *so, int locktype);
-#else
-void *sctp_getlock(struct socket *so, int locktype);
-#endif /* _KERN_LOCKS_H_ */
-void sctp_lock_assert(struct socket *so);
-void sctp_unlock_assert(struct socket *so);
-#endif /* SCTP_APPLE_FINE_GRAINED_LOCKING */
-
-/* emulate the BSD 'ticks' clock */
-extern int ticks;
-
-/* XXX: Hopefully temporary until APPLE changes to newer defs like other BSDs */
-#define if_addrlist	if_addrhead
-#define if_list		if_link
-#define ifa_list	ifa_link
 
 #endif
