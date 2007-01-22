5ab9a90d77e63662675567472b254cb062e9aaf5
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-01-22 09:09:49 +0000

    Sync with main sources.

--- trunk/KERN/nke/sctp_nke/Tiger/netinet/sctputil.c
+++ trunk/KERN/nke/sctp_nke/Tiger/netinet/sctputil.c
@@ -1471,7 +1471,7 @@ sctp_timeout_handler(void *t)
 			goto out_decr;
 		}
 		SCTP_STAT_INCR(sctps_timocookie);
-		stcb->asoc.timocookie;
+		stcb->asoc.timocookie++;
 #ifdef SCTP_AUDITING_ENABLED
 		sctp_auditing(4, inp, stcb, net);
 #endif
@@ -1518,7 +1518,7 @@ sctp_timeout_handler(void *t)
 			goto out_decr;
 		}
 		SCTP_STAT_INCR(sctps_timoshutdownack);
-		stcb->asoc.timoshutdownack;
+ 		stcb->asoc.timoshutdownack++;
 #ifdef SCTP_AUDITING_ENABLED
 		sctp_auditing(4, inp, stcb, net);
 #endif
