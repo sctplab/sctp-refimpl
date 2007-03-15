cd4916b9340a537fd54c997207e79e3f559f2ae1
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-03-15 11:41:50 +0000

    FreeBSD id sync.

--- KERN/netinet6/sctp6_usrreq.c
+++ KERN/netinet6/sctp6_usrreq.c
@@ -30,7 +30,7 @@
 /*	$KAME: sctp6_usrreq.c,v 1.38 2005/08/24 08:08:56 suz Exp $	*/
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet6/sctp6_usrreq.c,v 1.10 2007/02/12 23:24:31 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet6/sctp6_usrreq.c,v 1.11 2007/03/15 11:27:14 rrs Exp $");
 #endif
 
 #include <netinet/sctp_os.h>
