a98857b04a8370469e36fb9ea3f39869f46f8964
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-03-21 08:49:33 +0000

    Fix so that we only incr sent IF and only IF we are below RESEND... and fix missing places where we bump the number of packets needing retran.

--- trunk/KERN/netinet/sctp_input.c
+++ trunk/KERN/netinet/sctp_input.c
@@ -1315,7 +1315,7 @@ sctp_process_cookie_existing(struct mbuf *m, int iphlen, int offset,
 		        TAILQ_FOREACH(chk, &stcb->asoc.sent_queue, sctp_next) {
 				if(chk->sent < SCTP_DATAGRAM_RESEND) {
 					chk->sent = SCTP_DATAGRAM_RESEND;
-					stcb->asoc.sent_queue_retran_cnt++;
+					sctp_ucount_incr(stcb->asoc.sent_queue_retran_cnt);
 					spec_flag++;
 				}
 			}
