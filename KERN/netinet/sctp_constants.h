aeeab8586040d9c6aad6f6d26b845611e46bee16
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-04-06 10:03:37 +0000

    more diffences for flight logging.

--- KERN/netinet/sctp_constants.h
+++ KERN/netinet/sctp_constants.h
@@ -198,7 +198,9 @@ __FBSDID("$FreeBSD: src/sys/netinet/sctp_constants.h,v 1.10 2007/04/03 11:15:32 
 #define SCTP_FLIGHT_LOG_DOWN_RSND_TO    111
 #define SCTP_FLIGHT_LOG_DOWN_WP    112
 #define SCTP_FLIGHT_LOG_UP_REVOKE  113
-#define SCTP_LOG_MAX_TYPES 114
+#define SCTP_FLIGHT_LOG_DOWN_PDRP  114
+#define SCTP_FLIGHT_LOG_DOWN_PMTU  115
+#define SCTP_LOG_MAX_TYPES 116
 /*
  * To turn on various logging, you must first define SCTP_STAT_LOGGING. Then
  * to get something to log you define one of the logging defines i.e.
