793428f5594882ee9608efdf04e232cca864b4b6
lei <lei@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-02 22:11:23 +0000

    make getting v6 hop limit a macro

--- trunk/KERN/netinet/sctp_os_bsd.h
+++ trunk/KERN/netinet/sctp_os_bsd.h
@@ -304,6 +304,9 @@ typedef struct callout sctp_os_timer_t;
 #define SCTP_GET_IPV4_LENGTH(iph) (iph->ip_len)
 #define SCTP_GET_IPV6_LENGTH(ip6) (ntohs(ip6->ip6_plen))
 
+/* get the v6 hop limit */
+#define SCTP_GET_HLIM(inp, ro)	in6_selecthlim((struct in6pcb *)&inp->ip_inp.inp, (ro ? (ro->ro_rt ? (ro->ro_rt->rt_ifp) : (NULL)) : (NULL)));
+
 /* is the endpoint v6only? */
 #define SCTP_IPV6_V6ONLY(inp)	(((struct inpcb *)inp)->inp_flags & IN6P_IPV6_V6ONLY)
 /* is the socket non-blocking? */
