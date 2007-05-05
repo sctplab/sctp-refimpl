193e750db5154ea32df5df46d4835a0e2794f890
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-05 19:34:11 +0000

    Add FragPoint info to sysctl.

--- KERN/netinet/sctp_sysctl.c
+++ KERN/netinet/sctp_sysctl.c
@@ -223,6 +223,7 @@ sctp_assoclist(SYSCTL_HANDLER_ARGS)
 		xinpcb.total_sends            = inp->total_sends;
 		xinpcb.total_recvs            = inp->total_recvs;
 		xinpcb.total_nospaces         = inp->total_nospaces;
+		xinpcb.fragmentation_point    = inp->sctp_frag_point;
 		SCTP_INP_INCR_REF(inp);
 		SCTP_INP_RUNLOCK(inp);
 		SCTP_INP_INFO_RUNLOCK();
