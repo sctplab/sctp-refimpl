00ee20afd31271f9eb882529b338e0798502d752
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-06 00:10:00 +0000

    ID sync.

--- trunk/KERN/netinet/sctp_input.c
+++ trunk/KERN/netinet/sctp_input.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_input.c,v 1.21 2007/05/02 12:50:13 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_input.c,v 1.22 2007/05/06 00:01:17 rrs Exp $");
 #endif
 
 #include <netinet/sctp_os.h>
--- trunk/KERN/netinet/sctputil.c
+++ trunk/KERN/netinet/sctputil.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctputil.c,v 1.24 2007/05/03 14:42:42 rwatson Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctputil.c,v 1.25 2007/05/06 00:01:17 rrs Exp $");
 #endif
 
 #include <netinet/sctp_os.h>
@@ -1033,6 +1033,7 @@ sctp_init_asoc(struct sctp_inpcb *m, struct sctp_association *asoc,
 	asoc->my_rwnd = max(SCTP_SB_LIMIT_RCV(m->sctp_socket), SCTP_MINIMAL_RWND);
 	asoc->peers_rwnd = SCTP_SB_LIMIT_RCV(m->sctp_socket);
 
+	printf("Smallest MTU inits to %d\n", m->sctp_frag_point);
 	asoc->smallest_mtu = m->sctp_frag_point;
 	asoc->minrto = m->sctp_ep.sctp_minrto;
 	asoc->maxrto = m->sctp_ep.sctp_maxrto;
@@ -2575,6 +2576,8 @@ sctp_mtu_size_reset(struct sctp_inpcb *inp,
 	struct sctp_tmit_chunk *chk;
 	unsigned int eff_mtu, ovh;
 
+	printf("MTU re-size called mtu was:%d now:%d\n",
+	       asoc->smallest_mtu, mtu);
 	asoc->smallest_mtu = mtu;
 	if (inp->sctp_flags & SCTP_PCB_FLAGS_BOUND_V6) {
 		ovh = SCTP_MIN_OVERHEAD;
