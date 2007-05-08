be37303a81680ec9fb7766d70ca4ca496c52da84
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-08 16:04:35 +0000

    Copy write adjustment ", by " added and update to 2007 where missing.

--- trunk/KERN/netinet/sctp_lock_apple_fg.h
+++ trunk/KERN/netinet/sctp_lock_apple_fg.h
@@ -1,7 +1,7 @@
 #ifndef __sctp_lock_apple_fg_h__
 #define __sctp_lock_apple_fg_h__
 /*-
- * Copyright (c) 2001-2007, Cisco Systems, Inc. All rights reserved.
+ * Copyright (c) 2001-2007, by Cisco Systems, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
