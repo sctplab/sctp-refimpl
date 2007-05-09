210f42d2567f0a7376dd78bea1baab31a1fbc990
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-09 13:35:46 +0000

    FreeBSD ID sync.

--- KERN/netinet/sctp_asconf.c
+++ KERN/netinet/sctp_asconf.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_asconf.c,v 1.12 2007/05/08 17:01:10 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_asconf.c,v 1.13 2007/05/09 13:30:06 rrs Exp $");
 #endif
 #include <netinet/sctp_os.h>
 #include <netinet/sctp_var.h>
