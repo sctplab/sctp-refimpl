3622d8c9ed0186e66b14646e8858e9fc6f16b48a
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-15 21:12:06 +0000

    Get rid of sack_delay/sack_freq in assoc info.

--- APPS/apitester/src/sctp_utilities.h
+++ APPS/apitester/src/sctp_utilities.h
@@ -22,17 +22,13 @@ int sctp_get_assoc_info(int fd, sctp_assoc_t assoc_id,
 			uint16_t *peer_dest_cnt, 
 			uint32_t *peer_rwnd,
 			uint32_t *local_rwnd,
-			uint32_t *cookie_life,
-			uint32_t *sack_delay,
-			uint32_t *sack_freq);
+			uint32_t *cookie_life);
 int sctp_set_assoc_info(int fd, sctp_assoc_t assoc_id, 
 			uint16_t asoc_maxrxt,
 			uint16_t peer_dest_cnt, 
 			uint32_t peer_rwnd,
 			uint32_t local_rwnd,
-			uint32_t cookie_life,
-			uint32_t sack_delay,
-			uint32_t sack_freq);
+			uint32_t cookie_life);
 int sctp_set_asoc_maxrxt(int fd, sctp_assoc_t asoc, uint16_t max);
 int sctp_get_asoc_maxrxt(int fd, sctp_assoc_t asoc, uint16_t *max);
 
@@ -47,13 +43,6 @@ int sctp_get_asoc_local_rwnd(int fd, sctp_assoc_t asoc, uint32_t *lrwnd);
 
 int sctp_set_asoc_cookie_life(int fd, sctp_assoc_t asoc, uint32_t life);
 int sctp_get_asoc_cookie_life(int fd, sctp_assoc_t asoc, uint32_t *life);
-
-int sctp_set_asoc_sack_delay(int fd, sctp_assoc_t asoc, uint32_t delay);
-int sctp_get_asoc_sack_delay(int fd, sctp_assoc_t asoc, uint32_t *delay);
-
-int sctp_set_asoc_sack_freq(int fd, sctp_assoc_t asoc, uint32_t freq);
-int sctp_get_asoc_sack_freq(int fd, sctp_assoc_t asoc, uint32_t *freq);
-
 
 uint32_t sctp_get_number_of_associations(int);
 uint32_t sctp_get_association_identifiers(int, sctp_assoc_t [], unsigned int);
