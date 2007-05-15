7999ff54bc51c93db39385c2cba5813e0e6e9c9e
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-15 01:39:25 +0000

    two more tests.

--- trunk/APPS/apitester/src/sctp_utilities.h
+++ trunk/APPS/apitester/src/sctp_utilities.h
@@ -1,5 +1,6 @@
 int  sctp_one2one(unsigned short port, int should_listen);
 int sctp_socketpair(int *);
+int sctp_socketpair_reuse(int fd, int *fds);
 int sctp_socketstar(int *, int *, unsigned int);
 int sctp_shutdown(int);
 int sctp_abort(int);
