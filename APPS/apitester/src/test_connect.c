789b2554854f022a64ad24c0a8afaad0b1b88f8f
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-15 00:06:32 +0000

    Uses new utility in last part too.

--- APPS/apitester/src/test_connect.c
+++ APPS/apitester/src/test_connect.c
@@ -82,22 +82,10 @@ DEFINE_APITEST(connect, self_non_listen)
 	struct sockaddr_in addr;
 	socklen_t addr_len;
 	
-	if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) < 0)
+	fd = sctp_one2one(0, 0);
+	if (fd  < 0)
 		return strerror(errno);
-		
-	memset((void *)&addr, 0, sizeof(struct sockaddr_in));
-	addr.sin_family      = AF_INET;
-#ifdef HAVE_SIN_LEN
-	addr.sin_len         = sizeof(struct sockaddr_in);
-#endif
-	addr.sin_port        = htons(0);
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-	if (bind(fd, (struct sockaddr *)&addr, (socklen_t)sizeof(struct sockaddr_in)) < 0) {
-		close(fd);
-		return strerror(errno);
-	}
-	
 	addr_len = (socklen_t)sizeof(struct sockaddr_in);
 	if (getsockname (fd, (struct sockaddr *) &addr, &addr_len) < 0) {
 		close(fd);
@@ -123,27 +111,7 @@ DEFINE_APITEST(connect, self_listen)
 	struct sockaddr_in addr;
 	socklen_t addr_len;
 	
-	if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) < 0)
-		return strerror(errno);
-		
-	memset((void *)&addr, 0, sizeof(struct sockaddr_in));
-	addr.sin_family      = AF_INET;
-#ifdef HAVE_SIN_LEN
-	addr.sin_len         = sizeof(struct sockaddr_in);
-#endif
-	addr.sin_port        = htons(0);
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-	if (bind(fd, (struct sockaddr *)&addr, (socklen_t)sizeof(struct sockaddr_in)) < 0) {
-		close(fd);
-		return strerror(errno);
-	}
-	
-	if (listen(fd, 1) < 0) {
-		close(fd);
-		return strerror(errno);
-	}
-	
+	fd = sctp_one2one(0, 1);
 	addr_len = (socklen_t)sizeof(struct sockaddr_in);
 	if (getsockname(fd, (struct sockaddr *) &addr, &addr_len) < 0) {
 		close(fd);
