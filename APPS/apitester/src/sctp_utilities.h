015acd10b5a6d51e310ed2e299cb81aaa36beee4
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-18 22:12:35 +0000

    two more.

--- APPS/apitester/src/sctp_utilities.h
+++ APPS/apitester/src/sctp_utilities.h
@@ -1,4 +1,4 @@
-int  sctp_one2one(unsigned short port, int should_listen, int bindall);
+int sctp_one2one(unsigned short port, int should_listen, int bindall);
 int sctp_socketpair(int *, int bindall);
 int sctp_socketpair_reuse(int fd, int *fds, int bindall);
 int sctp_socketstar(int *, int *, unsigned int);
