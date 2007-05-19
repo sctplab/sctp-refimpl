9aa07f81a3053242f1c749f3c72781536b66a332
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-19 21:30:34 +0000

    Add some more tests.

--- trunk/APPS/apitester/src/test_sctp_bindx.c
+++ trunk/APPS/apitester/src/test_sctp_bindx.c
@@ -218,3 +218,107 @@ DEFINE_APITEST(bindx, rem_zero_addresses_NULL)
 	else
 		return NULL;
 }
+
+DEFINE_APITEST(bindx, dup_add_s_a_s_p)
+{
+	int fd, result;
+	struct sockaddr_in address;
+	
+	if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) < 0)
+		return strerror(errno);
+		
+	bzero((void *)&address, sizeof(struct sockaddr_in));
+	address.sin_family = AF_INET;
+#ifdef HAVE_SIN_LEN
+	address.sin_len = sizeof(struct sockaddr_in);
+#endif
+	address.sin_port = htons(1234);
+	address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	
+	if (sctp_bindx(fd, (struct sockaddr *)&address, 1, SCTP_BINDX_ADD_ADDR) < 0) {
+		close(fd);
+		return strerror(errno);
+	}
+	
+	result = sctp_bindx(fd, (struct sockaddr *)&address, 1, SCTP_BINDX_ADD_ADDR);
+	close(fd);
+		
+	if (result)
+		return strerror(errno);
+	else
+		return NULL;
+}
+
+DEFINE_APITEST(bindx, rem_last_s_a_s_p)
+{
+	int fd, result;
+	struct sockaddr_in address;
+	
+	if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) < 0)
+		return strerror(errno);
+		
+	bzero((void *)&address, sizeof(struct sockaddr_in));
+	address.sin_family = AF_INET;
+#ifdef HAVE_SIN_LEN
+	address.sin_len = sizeof(struct sockaddr_in);
+#endif
+	address.sin_port = htons(1234);
+	address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	
+	if (sctp_bindx(fd, (struct sockaddr *)&address, 1, SCTP_BINDX_ADD_ADDR) < 0) {
+		close(fd);
+		return strerror(errno);
+	}
+	
+	result = sctp_bindx(fd, (struct sockaddr *)&address, 1, SCTP_BINDX_REM_ADDR);
+	close(fd);
+		
+	if (result)
+		return NULL;
+	else
+		return "Can remove last address";
+}
+
+DEFINE_APITEST(bindx, rem_s_a_s_p)
+{
+	int fd, result;
+	struct sockaddr_in address;
+	
+	if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) < 0)
+		return strerror(errno);
+		
+	bzero((void *)&address, sizeof(struct sockaddr_in));
+	address.sin_family = AF_INET;
+#ifdef HAVE_SIN_LEN
+	address.sin_len = sizeof(struct sockaddr_in);
+#endif
+	address.sin_port = htons(1234);
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if (sctp_bindx(fd, (struct sockaddr *)&address, 1, SCTP_BINDX_ADD_ADDR) < 0) {
+		close(fd);
+		return strerror(errno);
+	}
+
+	bzero((void *)&address, sizeof(struct sockaddr_in));
+	address.sin_family = AF_INET;
+#ifdef HAVE_SIN_LEN
+	address.sin_len = sizeof(struct sockaddr_in);
+#endif
+	address.sin_port = htons(1234);
+	address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	
+	result = sctp_bindx(fd, (struct sockaddr *)&address, 1, SCTP_BINDX_REM_ADDR);
+	close(fd);
+		
+	if (result)
+		return strerror(errno);
+	else
+		return NULL;
+}
+
+/*
+ * packed/unpacked.
+ * v6only
+ * multiple values.
+ */
