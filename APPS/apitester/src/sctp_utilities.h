556838ee442b427b9cb4477323a05b6cc3b5aa01
lei <lei@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-05-19 19:35:52 +0000

    helper functions for auth key socket opts

--- APPS/apitester/src/sctp_utilities.h
+++ APPS/apitester/src/sctp_utilities.h
@@ -163,11 +163,16 @@ int sctp_set_auth_chunk_id(int fd, uint8_t chk);
  * SCTP_KEY tests
  *
  ********************************************************/
+int sctp_get_add_key(int fd, sctp_assoc_t assoc_id, uint16_t keyid,
+		     uint16_t *keylen, uint8_t *keytext);
+int sctp_set_add_key(int fd, sctp_assoc_t assoc_id, uint16_t keyid,
+		     uint16_t keylen, uint8_t *keytext);
 
+int sctp_get_active_key(int fd, sctp_assoc_t assoc_id, uint16_t *keyid);
+int sctp_set_active_key(int fd, sctp_assoc_t assoc_id, uint16_t keyid);
 
-
-
-
+int sctp_get_delete_key(int fd, sctp_assoc_t assoc_id, uint16_t *keyid);
+int sctp_set_delete_key(int fd, sctp_assoc_t assoc_id, uint16_t keyid);
 
 /********************************************************
  *
