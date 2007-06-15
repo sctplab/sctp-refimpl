6b4de31e9fd714355804017980f720a39c9e00d3
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-06-15 18:20:04 +0000

    Botth init and destroy other inp lock :-0

--- trunk/KERN/netinet/sctp_pcb.c
+++ trunk/KERN/netinet/sctp_pcb.c
@@ -2277,7 +2277,7 @@ sctp_inpcb_alloc(struct socket *so, uint32_t vrf_id)
 	SCTP_INP_INFO_WLOCK();
 	SCTP_INP_LOCK_INIT(inp);
 #if defined(__FreeBSD__)
-	INP_LOCK_INIT(&inp.ip_inp.inp, "inp", "sctpinp");
+	INP_LOCK_INIT(&inp->ip_inp.inp, "inp", "sctpinp");
 #endif
 	SCTP_INP_READ_INIT(inp);
 	SCTP_ASOC_CREATE_LOCK_INIT(inp);
@@ -3611,6 +3611,9 @@ sctp_inpcb_free(struct sctp_inpcb *inp, int immediate, int from)
 		inp->sctp_tcbhash = NULL;
 	}
 	/* Now we must put the ep memory back into the zone pool */
+#if defined(__FreeBSD__)
+	INP_LOCK_DESTROY(&inp->ip_inp.inp);
+#endif
 	SCTP_INP_LOCK_DESTROY(inp);
 	SCTP_INP_READ_DESTROY(inp);
 	SCTP_ASOC_CREATE_LOCK_DESTROY(inp);
