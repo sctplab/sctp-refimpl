dbde26fac5c29968c50a39aa144af7a1f687a1dc
lei <lei@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-06-15 17:56:16 +0000

    fix a few comment blocks

--- trunk/KERN/netinet/sctp_input.c
+++ trunk/KERN/netinet/sctp_input.c
@@ -3689,16 +3689,17 @@ sctp_process_control(struct mbuf *m, int iphlen, int *offset, int length,
 				return (NULL);
 			}
 		} else {
-			/* For cookies and all other chunks.
-			 * if the 
-			 */
+			/* For cookies and all other chunks. */
 			if (chk_length > sizeof(chunk_buf)) {
-				/* use just the size of the chunk buffer
+				/*
+				 * use just the size of the chunk buffer
 				 * so the front part of our chunks fit in
-				 * contiguous space up to the chunk buffer size (508 bytes).
-				 * For chunks that need to get more than that they mus
-				 * use the sctp_m_getptr() function or other means (know
-				 * how to parse mbuf chains). Cookies do this already.
+				 * contiguous space up to the chunk buffer
+				 * size (508 bytes).
+				 * For chunks that need to get more than that
+				 * they must use the sctp_m_getptr() function
+				 * or other means (e.g. know how to parse mbuf
+				 * chains). Cookies do this already.
 				 */
 				ch = (struct sctp_chunkhdr *)sctp_m_getptr(m, *offset,
 									   (sizeof(chunk_buf) - 4), 
