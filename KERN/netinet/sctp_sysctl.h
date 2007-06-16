a71565b7fe3d71b768630659c26f5819053a1b58
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-06-16 03:29:20 +0000

    ID sync.

--- KERN/netinet/sctp_sysctl.h
+++ KERN/netinet/sctp_sysctl.h
@@ -30,7 +30,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_sysctl.h,v 1.6 2007/06/14 22:59:03 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_sysctl.h,v 1.7 2007/06/16 03:28:18 rrs Exp $");
 #endif
 
 #ifndef __sctp_sysctl_h__
