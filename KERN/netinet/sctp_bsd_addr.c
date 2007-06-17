8ebff4d96ff35b842471c14c5f299c0ff4741d1e
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-06-17 23:44:31 +0000

    idsync

--- KERN/netinet/sctp_bsd_addr.c
+++ KERN/netinet/sctp_bsd_addr.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_bsd_addr.c,v 1.11 2007/06/14 22:59:02 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_bsd_addr.c,v 1.12 2007/06/17 23:43:37 rrs Exp $");
 #endif
 
 #include <netinet/sctp_os.h>
