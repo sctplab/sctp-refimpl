bcb64c1bee4951b4427480ecc871d76e4a1e8094
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-06-20 11:11:33 +0000

    take out debug.

--- trunk/KERN/netinet/sctputil.c
+++ trunk/KERN/netinet/sctputil.c
@@ -2749,9 +2749,6 @@ sctp_m_getptr(struct mbuf *m, int off, int len, uint8_t * in_ptr)
 {
 	uint32_t count;
 	uint8_t *ptr;
-	int maximum, cnt=0;
-	struct mbuf *prev=NULL;
-	maximum = (65536/MLEN) + 100;
 
 	ptr = in_ptr;
 	if ((off < 0) || (len <= 0))
@@ -2759,19 +2756,9 @@ sctp_m_getptr(struct mbuf *m, int off, int len, uint8_t * in_ptr)
 
 	/* find the desired start location */
 	while ((m != NULL) && (off > 0)) {
-		cnt++;
-		if (cnt > maximum) {
-			SCTP_PRINTF("at cnt:%d prev:%p m:%p m_len:%d - terminate\n",
-				   cnt, prev, m, SCTP_BUF_LEN(m));
-			if(prev == m) {
-				panic ("particle chain corrupted");
-			}
-			return(NULL);
-		}
 		if (off < SCTP_BUF_LEN(m))
 			break;
 		off -= SCTP_BUF_LEN(m);
-		prev = m;
 		m = SCTP_BUF_NEXT(m);
 	}
 	if (m == NULL)
