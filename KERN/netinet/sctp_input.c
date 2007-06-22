4e3af76c5f7fef02b36b3f090c4d19a22002ca7c
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-06-22 14:40:52 +0000

    ID sync.

--- KERN/netinet/sctp_input.c
+++ KERN/netinet/sctp_input.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_input.c,v 1.45 2007/06/22 13:50:56 rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_input.c,v 1.46 2007/06/22 14:40:09 rrs Exp $");
 #endif
 
 #include <netinet/sctp_os.h>
