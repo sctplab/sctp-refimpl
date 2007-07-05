dc7a4de4ff64e74ad87140a690b50ae57b0b5c63
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-07-05 22:24:22 +0000

    more check

--- trunk/KERN/netinet/sctputil.c
+++ trunk/KERN/netinet/sctputil.c
@@ -1572,9 +1572,19 @@ sctp_timeout_handler(void *t)
 #if defined(SCTP_PER_SOCKET_LOCKING)
 		sctp_lock_assert(SCTP_INP_SO(stcb->sctp_ep));
 #endif
-		atomic_add_int(&stcb->asoc.refcnt, -1);
+		if ((stcb->asoc.state == 0) ||
+		    (stcb->asoc.state & SCTP_STATE_ABOUT_TO_BE_FREED)) {
+			atomic_add_int(&stcb->asoc.refcnt, -1);
+			SCTP_TCB_UNLOCK(stcb);
+			if (inp) {
+#if defined(SCTP_PER_SOCKET_LOCKING)
+				SCTP_SOCKET_UNLOCK(SCTP_INP_SO(inp), 1);
+#endif
+				SCTP_INP_DECR_REF(inp);
+			}
+			return;
+		}
 	}
-
 	/* record in stopped what t-o occured */
 	tmr->stopped_from = tmr->type;
 
