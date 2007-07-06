f80fdab9906a44de5c20ce32d1fb78c29a3e9a5f
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-07-06 11:56:20 +0000

    asoc kill timer must be exempt from check.

--- KERN/netinet/sctputil.c
+++ KERN/netinet/sctputil.c
@@ -1573,8 +1573,9 @@ sctp_timeout_handler(void *t)
 		sctp_lock_assert(SCTP_INP_SO(stcb->sctp_ep));
 #endif
 		atomic_add_int(&stcb->asoc.refcnt, -1);
-		if ((stcb->asoc.state == 0) ||
-		    (stcb->asoc.state & SCTP_STATE_ABOUT_TO_BE_FREED)) {
+		if ((tmr->type != SCTP_TIMER_TYPE_ASOCKILL) &&
+		    ((stcb->asoc.state == 0) ||
+		     (stcb->asoc.state & SCTP_STATE_ABOUT_TO_BE_FREED))) {
 			SCTP_TCB_UNLOCK(stcb);
 			if (inp) {
 #if defined(SCTP_PER_SOCKET_LOCKING)
