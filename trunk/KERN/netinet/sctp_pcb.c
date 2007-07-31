b1ac6ae91a78bb69da26883197edbed769933b83
lei <lei@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-07-31 16:39:18 +0000

    If a socket is closed while a partial delivery is in progress, a panic may occur when the assoc that is doing PD is freed; queing the partial delivery aborted notify may dereference a null socket pointer.

--- trunk/KERN/netinet/sctp_pcb.c
+++ trunk/KERN/netinet/sctp_pcb.c
@@ -4569,7 +4569,7 @@ sctp_free_assoc(struct sctp_inpcb *inp, struct sctp_tcb *stcb, int from_inpcbfre
 					/* Held for PD-API clear that. */
 					sq->pdapi_aborted = 1;
 					sq->held_length = 0;
-					if (sctp_is_feature_on(inp, SCTP_PCB_FLAGS_PDAPIEVNT)) {
+					if (sctp_is_feature_on(inp, SCTP_PCB_FLAGS_PDAPIEVNT) && (so != NULL)) {
 						/*
 						 * Need to add a PD-API aborted indication.
 						 * Setting the control_pdapi assures that it will
