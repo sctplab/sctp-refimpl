6aa5f1b60863c1a1f6b265de7ac7eef590529479
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-08-01 18:38:49 +0000

    Just sync my machines.

--- KERN/nke/sctp_nke/Tiger/netinet/sctp_usrreq.c
+++ KERN/nke/sctp_nke/Tiger/netinet/sctp_usrreq.c
@@ -326,17 +326,16 @@ sctp_notify(struct sctp_inpcb *inp,
 			 */
 			sctp_abort_notification(stcb, SCTP_PEER_FAULTY);
 #if defined(__APPLE__)
-			if (stcb) {
-				atomic_add_int(&stcb->asoc.refcnt, 1);
-				SCTP_TCB_UNLOCK(stcb);
-				SCTP_TCB_LOCK(stcb);
-				atomic_subtract_int(&stcb->asoc.refcnt, 1);
-			}
+			atomic_add_int(&stcb->asoc.refcnt, 1);
+			SCTP_TCB_UNLOCK(stcb);
 			SCTP_SOCKET_LOCK(SCTP_INP_SO(inp), 1);
+			SCTP_TCB_LOCK(stcb);
 #endif
 			sctp_free_assoc(inp, stcb, SCTP_NORMAL_PROC, SCTP_FROM_SCTP_USRREQ+SCTP_LOC_2);
 #if defined (__APPLE__)
 			SCTP_SOCKET_UNLOCK(SCTP_INP_SO(inp), 1);
+			atomic_subtract_int(&stcb->asoc.refcnt, 1);
+			SCTP_TCB_UNLOCK(stcb);
 #endif
 			/* no need to unlock here, since the TCB is gone */
 		}
