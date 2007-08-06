8be62a20d4007b13fee860c1a0c036af5be48118
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-08-06 16:11:31 +0000

    BSD ID sync..
    Note PLEASE DO NOT REMOVE extra spaces in the code.. THIS JUST
    CAUSES extra BSD commits and Churns... I took out some of
    this so I would not have futher RE patches..

--- trunk/KERN/netinet/sctp_input.c
+++ trunk/KERN/netinet/sctp_input.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_input.c,v 1.54 2007/08/06 14:26:01 rwatson Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_input.c,v 1.55 2007/08/06 15:46:46 rrs Exp $");
 #endif
 
 #include <netinet/sctp_os.h>
