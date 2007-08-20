5a069eabd7352684139c612d99ffe2a7a9eb2e5e
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-08-20 06:07:25 +0000

    Fix roll over bug.

--- trunk/KERN/netinet/sctputil.c
+++ trunk/KERN/netinet/sctputil.c
@@ -6741,11 +6741,12 @@ sctp_log_trace(uint32_t subsys, const char *str, uint32_t a, uint32_t b, uint32_
 	int saveindex, newindex;
 	do {
 		saveindex = sctp_log_index;
-		newindex = saveindex + 1;
-		if(saveindex > SCTP_MAX_LOGGING_SIZE) {
+		if(saveindex >= SCTP_MAX_LOGGING_SIZE) {
 			saveindex = 0;
+		} else {
+			newindex = saveindex + 1;
 		}
-	} while (atomic_cmpset_rel_int(&sctp_log_index, saveindex, newindex) == 0);
+	} while (atomic_cmpset_int(&sctp_log_index, saveindex, newindex) == 0);
 	sctp_log[saveindex].timestamp = SCTP_GET_CYCLECOUNT;
 	sctp_log[saveindex].subsys = subsys;
 	sctp_log[saveindex].descr = str;
