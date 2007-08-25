aefbd7cedc31cbb8009967f32a7f0f7981c0a815
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-08-25 04:50:02 +0000

    AQdd the interop scripts.

---
+++ trunk/interop/config_pipes.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Cisco
+ipfw pipe 1 config plr 0.0 delay 0ms
+ipfw pipe 2 config plr 0.0 delay 0ms
+
+# Continuous Computing
+ipfw pipe 3 config plr 0.0 delay 0ms
+ipfw pipe 4 config plr 0.0 delay 0ms
+
+# Emerson
+ipfw pipe 5 config plr 0.0 delay 0ms
+ipfw pipe 6 config plr 0.0 delay 0ms
+
+# HP
+ipfw pipe 7 config plr 0.0 delay 0ms
+ipfw pipe 8 config plr 0.0 delay 0ms
+
+# Korean Naional University
+ipfw pipe 9 config plr 0.0 delay 0ms
+ipfw pipe 10 config plr 0.0 delay 0ms
+
+# Kyoto University
+ipfw pipe 11 config plr 0.0 delay 0ms
+ipfw pipe 12 config plr 0.0 delay 0ms
+
+# Muenster University of Applied Sciences
+ipfw pipe 13 config plr 0.0 delay 0ms
+ipfw pipe 14 config plr 0.0 delay 0ms
+
+# Sun
+ipfw pipe 15 config plr 0.0 delay 0ms
+ipfw pipe 16 config plr 0.0 delay 0ms
+
+# University of Duisburg/Essen
+ipfw pipe 17 config plr 0.0 delay 0ms
+ipfw pipe 18 config plr 0.0 delay 0ms
