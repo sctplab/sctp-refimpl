6483da1eb333b5e1ee9ed24faa553aeff6926e46
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-09-10 21:02:42 +0000

    Patch 21 id sync.

--- KERN/netinet/sctp_asconf.c
+++ KERN/netinet/sctp_asconf.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet/sctp_asconf.c,v 1.30 2007/09/1? ??:??:?? rrs Exp $");
+__FBSDID("$FreeBSD: src/sys/netinet/sctp_asconf.c,v 1.30 2007/09/10 21:01:56 rrs Exp $");
 #endif
 #include <netinet/sctp_os.h>
 #include <netinet/sctp_var.h>
