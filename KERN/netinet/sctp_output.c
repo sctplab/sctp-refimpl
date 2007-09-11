6f38dc090502803b631d36b6b3fb29970b772323
lei <lei@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-09-11 22:32:16 +0000

    fix for mbuf send case and SCTP_EOF flag...

--- KERN/netinet/sctp_output.c
+++ KERN/netinet/sctp_output.c
@@ -12493,6 +12493,15 @@ sctp_lower_sosend(struct socket *so,
 		/* We send in a 0, since we do NOT have any locks */
 		error = sctp_msg_append(stcb, net, top, srcv, 0);
 		top = NULL;
+		if (srcv->sinfo_flags & SCTP_EOF) {
+			/*
+			 * This should only happen for Panda for the mbuf
+			 * send case, which does NOT yet support EEOR mode.
+			 * Thus, we can just set this flag to do the proper
+			 * EOF handling.
+			 */
+			got_all_of_the_send = 1;
+		}
 	}
 	if (error) {
 		goto out;
