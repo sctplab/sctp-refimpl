0ad62d7e2dc977db924f43c84884d136b56178ea
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2007-09-12 21:14:07 +0000

    Get the dump via sysctl mechnism.

--- trunk/APPS/debug_tools/dump_apple_log.c
+++ trunk/APPS/debug_tools/dump_apple_log.c
@@ -1,39 +1,33 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/errno.h>
-
-struct sctp_dump_log {
-	u_int64_t timestamp;
-	const char *descr;
-	uint32_t subsys;
-	uint32_t params[6];
-};
+#include <netinet/sctp.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/sysctl.h>
+#include <string.h>
 
 int
 main(int argc, char **argv)
 {
-	FILE *io;
-	struct sctp_dump_log log;
-	int cnt=0;
-	if(argc < 2) {
-		printf("Error use %s file\n", argv[0]);
-		return (-1);
+	struct sctp_log log;
+	unsigned int i;
+	size_t len = sizeof(struct sctp_log);
+
+	if (sysctlbyname("net.inet.sctp.log", &log, &len, NULL, 0) < 0) {
+		printf("Error %d (%s) could not get the log.\n", errno, strerror(errno));
+		return(0);
 	}
-	io = fopen(argv[1], "r");
-	if(io == NULL) {
-		printf("Can't open trace file %s err:%d\n", argv[1], errno);
-	}
-	while(fread(&log, sizeof(log), 1, io) > 0) {
+
+	for (i = 0; i < SCTP_MAX_LOGGING_SIZE; i++) {
 		printf("%d %llu SCTP:%d[%d]:%x-%x-%x-%x\n",
-		       cnt,
-		       log.timestamp,
-		       log.params[0],
-		       log.params[1],
-		       log.params[2],
-		       log.params[3],
-		       log.params[4],
-		       log.params[5]);
+		       i,
+		       log.entry[i].timestamp,
+		       log.entry[i].params[0],
+		       log.entry[i].params[1],
+		       log.entry[i].params[2],
+		       log.entry[i].params[3],
+		       log.entry[i].params[4],
+		       log.entry[i].params[5]);
 	}
-	fclose(io);
 	return (0);
 }
