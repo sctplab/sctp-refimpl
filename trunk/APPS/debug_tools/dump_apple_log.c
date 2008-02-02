6593da1b0abf507839c7c0dd1d123e1688fd81b6
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2008-02-02 11:13:32 +0000

    fixes so if logging is not enabled it still compiles

--- trunk/APPS/debug_tools/dump_apple_log.c
+++ trunk/APPS/debug_tools/dump_apple_log.c
@@ -5,13 +5,19 @@
 #include <errno.h>
 #include <sys/sysctl.h>
 #include <string.h>
-
+#ifndef SCTP_MAX_LOGGING_SIZE
+#define SCTP_MAX_LOGGING_SIZE 0
+#define NO_LOGGING_PRINT 1}
+/* this means the logging option is disabled */
+#endif
 int
 main(int argc, char **argv)
 {
+#ifndef NO_LOGGING_PRINT
 	struct sctp_log log;
 	unsigned int i;
 	size_t len = sizeof(struct sctp_log);
+
 
 	if (sysctlbyname("net.inet.sctp.log", &log, &len, NULL, 0) < 0) {
 		printf("Error %d (%s) could not get the log.\n", errno, strerror(errno));
@@ -29,5 +35,8 @@ main(int argc, char **argv)
 		       log.entry[i].params[4],
 		       log.entry[i].params[5]);
 	}
+#else
+        printf("Apple dump logging not supported\n");
+#endif
 	return (0);
 }
