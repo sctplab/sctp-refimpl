3bff8fadb2705d6c3d6e1f29db4c3b622877abe3
ma-kun <ma-kun@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2008-03-10 23:38:30 +0000

    - Add logging for mbuf. This helps to find memory leak bug. - Fix a problem around calculating of maximum length for send.

--- KERN/netinet/sctp_output.c
+++ KERN/netinet/sctp_output.c
@@ -3194,6 +3194,18 @@ sctp_add_cookie(struct sctp_inpcb *inp, struct mbuf *init, int init_offset,
 		sctp_m_freem(mret);
 		return (NULL);
 	}
+#ifdef SCTP_MBUF_LOGGING
+	if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+		struct mbuf *mat;
+		mat = copy_init;
+		while (mat) {
+			if (SCTP_BUF_IS_EXTENDED(mat)) {
+				sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+			}
+			mat = SCTP_BUF_NEXT(mat);
+		}
+	}
+#endif
 	copy_initack = SCTP_M_COPYM(initack, initack_offset, M_COPYALL,
 	    M_DONTWAIT);
 	if (copy_initack == NULL) {
@@ -3201,6 +3213,18 @@ sctp_add_cookie(struct sctp_inpcb *inp, struct mbuf *init, int init_offset,
 		sctp_m_freem(copy_init);
 		return (NULL);
 	}
+#ifdef SCTP_MBUF_LOGGING
+	if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+		struct mbuf *mat;
+		mat = copy_initack;
+		while (mat) {
+			if (SCTP_BUF_IS_EXTENDED(mat)) {
+				sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+			}
+			mat = SCTP_BUF_NEXT(mat);
+		}
+	}
+#endif
 	/* easy side we just drop it on the end */
 	ph = mtod(mret, struct sctp_paramhdr *);
 	SCTP_BUF_LEN(mret) = sizeof(struct sctp_state_cookie) +
@@ -5780,6 +5804,18 @@ sctp_copy_mbufchain(struct mbuf *clonechain,
 		} else {
 			/* copy the old fashion way */
 			appendchain = SCTP_M_COPYM(clonechain, 0, M_COPYALL, M_DONTWAIT);
+#ifdef SCTP_MBUF_LOGGING
+			if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+				struct mbuf *mat;
+				mat = appendchain;
+				while (mat) {
+					if (SCTP_BUF_IS_EXTENDED(mat)) {
+						sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+					}
+					mat = SCTP_BUF_NEXT(mat);
+				}
+			}
+#endif
 		}
 	}
 	if (appendchain == NULL) {
@@ -5867,6 +5903,18 @@ sctp_sendall_iterator(struct sctp_inpcb *inp, struct sctp_tcb *stcb, void *ptr,
 			ca->cnt_failed++;
 			return;
 		}
+#ifdef SCTP_MBUF_LOGGING
+		if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+			struct mbuf *mat;
+			mat = m;
+			while (mat) {
+				if (SCTP_BUF_IS_EXTENDED(mat)) {
+					sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+				}
+				mat = SCTP_BUF_NEXT(mat);
+			}
+		}
+#endif
 	} else {
 		m = NULL;
 	}
@@ -6588,6 +6636,18 @@ sctp_move_to_outqueue(struct sctp_tcb *stcb, struct sctp_nets *net,
 	  to_move = 0;
 	  goto out_of;
 	}
+#ifdef SCTP_MBUF_LOGGING
+	if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+		struct mbuf *mat;
+		mat = chk->data;
+		while (mat) {
+			if (SCTP_BUF_IS_EXTENDED(mat)) {
+				sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+			}
+			mat = SCTP_BUF_NEXT(mat);
+		}
+	}
+#endif
 	/* Pull off the data */
 	m_adj(sp->data, to_move);
 	/* Now lets work our way down and compact it */
@@ -7948,6 +8008,18 @@ sctp_send_cookie_echo(struct mbuf *m,
 				/* No memory */
 				return (-2);
 			}
+#ifdef SCTP_MBUF_LOGGING
+			if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+				struct mbuf *mat;
+				mat = cookie;
+				while (mat) {
+					if (SCTP_BUF_IS_EXTENDED(mat)) {
+						sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+					}
+					mat = SCTP_BUF_NEXT(mat);
+				}
+			}
+#endif
 			break;
 		}
 		at += SCTP_SIZE32(plen);
@@ -8009,6 +8081,18 @@ sctp_send_heartbeat_ack(struct sctp_tcb *stcb,
 		/* gak out of memory */
 		return;
 	}
+#ifdef SCTP_MBUF_LOGGING
+	if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+		struct mbuf *mat;
+		mat = outchain;
+		while (mat) {
+			if (SCTP_BUF_IS_EXTENDED(mat)) {
+				sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+			}
+			mat = SCTP_BUF_NEXT(mat);
+		}
+	}
+#endif
 	chdr = mtod(outchain, struct sctp_chunkhdr *);
 	chdr->chunk_type = SCTP_HEARTBEAT_ACK;
 	chdr->chunk_flags = 0;
@@ -8265,6 +8349,18 @@ sctp_send_asconf_ack(struct sctp_tcb *stcb)
 			/* couldn't copy it */
 			return;
 		}
+#ifdef SCTP_MBUF_LOGGING
+		if (sctp_logging_level & SCTP_MBUF_LOGGING_ENABLE) {
+			struct mbuf *mat;
+			mat = m_ack;
+			while (mat) {
+				if (SCTP_BUF_IS_EXTENDED(mat)) {
+					sctp_log_mb(mat, SCTP_MBUF_ICOPY);
+				}
+				mat = SCTP_BUF_NEXT(mat);
+			}
+		}
+#endif
 
 		sctp_alloc_a_chunk(stcb, chk);
 		if (chk == NULL) {
@@ -12000,7 +12096,7 @@ sctp_lower_sosend(struct socket *so,
 	  /* we already checked for non-blocking above. */
 	  max_len = sndlen;
 	} else {
-	  max_len = SCTP_SB_LIMIT_SND(so) -  stcb->asoc.total_output_queue_size;
+	  max_len = SCTP_SB_LIMIT_SND(so) - inqueue_bytes;
 	}
   } else {
 	max_len = 0;
