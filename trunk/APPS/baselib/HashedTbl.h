f7494ac64160379025687c38359f38be83e12dd0
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2008-05-08 14:10:51 +0000

    Makes the .h files consistent with the same BSD liscense that is in the .c files :-)

--- trunk/APPS/baselib/HashedTbl.h
+++ trunk/APPS/baselib/HashedTbl.h
@@ -1,42 +1,34 @@
-/* SCTP reference Implementation Copyright (C) 2001 Cisco
-
-This file is part of the SCTP reference Implementation
-
-Version:4.0.5
-$Header: /usr/sctpCVS/APPS/baselib/HashedTbl.h,v 1.2 2005-01-18 17:41:55 randall Exp $
-
-
-The SCTP reference implementation  is free software; 
-you can redistribute it and/or modify it under the terms of 
-the GNU Library General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-the SCTP reference implementation  is distributed in the hope that it 
-will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-                ************************
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Library General Public License for more details.
-
-You should have received a copy of the GNU Library General Public License
-along with GNU CC; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  
-
-Please send any bug reports or fixes you make to one of the following email
-addresses:
-
-rrs@cisco.com
-kmorneau@cisco.com
-
-Any bugs reported given to us we will try to fix... any fixes shared will
-be incorperated into the next SCTP release.
-
-There are still LOTS of bugs in this code... I always run on the motto
-"it is a wonder any code ever works :)"
+/*
+  $Header: /usr/sctpCVS/APPS/baselib/HashedTbl.h,v 1.3 2008-05-08 14:10:51 randall Exp $
+ * Copyright (C) 2002 Cisco Systems Inc,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 
-*/
 #ifndef __HashedTbl_h__
 #define __HashedTbl_h__
 
--- trunk/APPS/baselib/dlist.h
+++ trunk/APPS/baselib/dlist.h
@@ -2,16 +2,38 @@
 
 This file is part of the SCTP reference Implementation
 
-Version:4.0.5
-$Header: /usr/sctpCVS/APPS/baselib/dlist.h,v 1.1.1.1 2004-06-23 13:07:29 randall Exp $
+$Header: /usr/sctpCVS/APPS/baselib/dlist.h,v 1.2 2008-05-08 14:10:51 randall Exp $
+*/
+/*
+ * Copyright (C) 2002 Cisco Systems Inc,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
-
-The SCTP reference implementation  is free software; 
-you can redistribute it and/or modify it under the terms of 
-the GNU Library General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
+/*
 the SCTP reference implementation  is distributed in the hope that it 
 will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
                 ************************
