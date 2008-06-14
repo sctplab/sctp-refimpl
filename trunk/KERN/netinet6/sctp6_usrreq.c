503ebb26a1b76986af730935d9f975de1e81bbee
rrs <rrs@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2008-06-14 07:18:15 +0000

    Get the inet6 ones too.

--- trunk/KERN/netinet6/sctp6_usrreq.c
+++ trunk/KERN/netinet6/sctp6_usrreq.c
@@ -31,7 +31,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/netinet6/sctp6_usrreq.c,v 1.44 2008/05/20 13:47:46 rrs Exp $");
+__FBSDID("$FreeBSD: head/sys/netinet6/sctp6_usrreq.c 179157 2008-05-20 13:47:46Z rrs $");
 #endif
 
 #include <netinet/sctp_os.h>
