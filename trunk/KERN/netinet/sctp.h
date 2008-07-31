9a9a4535f317763e994ea4f4d288cce96354b362
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2008-07-31 12:35:54 +0000

    fix sync hrr

--- trunk/KERN/netinet/sctp.h
+++ trunk/KERN/netinet/sctp.h
@@ -31,8 +31,9 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 181054 2008-07-31 11:08:30Z rrs $");#endif
+__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 181054 2008-07-31 11:08:30Z rrs $");
 
+#endif
 #ifndef _NETINET_SCTP_H_
 #define _NETINET_SCTP_H_
 
