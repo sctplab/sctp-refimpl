ad6816109188714b396b35352dfa2d5cf6f16e3d
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2008-11-09 20:56:34 +0000

    Sync with main tree.

--- trunk/KERN/nke/sctp_nke/Tiger/netinet/sctputil.c
+++ trunk/KERN/nke/sctp_nke/Tiger/netinet/sctputil.c
@@ -1469,12 +1469,13 @@ sctp_timeout_handler(void *t)
 	if (inp) {
 		SCTP_INP_INCR_REF(inp);
 		if ((inp->sctp_socket == 0) && 
-		    ((tmr->type !=  SCTP_TIMER_TYPE_INPKILL) &&
-			 (tmr->type !=  SCTP_TIMER_TYPE_SEND) &&
-			 (tmr->type !=  SCTP_TIMER_TYPE_HEARTBEAT) &&
-		     (tmr->type !=  SCTP_TIMER_TYPE_SHUTDOWN) &&
-		     (tmr->type !=  SCTP_TIMER_TYPE_SHUTDOWNACK) &&
-		     (tmr->type !=  SCTP_TIMER_TYPE_SHUTDOWNGUARD) &&
+		    ((tmr->type != SCTP_TIMER_TYPE_INPKILL) &&
+		     (tmr->type != SCTP_TIMER_TYPE_SEND) &&
+		     (tmr->type != SCTP_TIMER_TYPE_RECV) &&
+		     (tmr->type != SCTP_TIMER_TYPE_HEARTBEAT) &&
+		     (tmr->type != SCTP_TIMER_TYPE_SHUTDOWN) &&
+		     (tmr->type != SCTP_TIMER_TYPE_SHUTDOWNACK) &&
+		     (tmr->type != SCTP_TIMER_TYPE_SHUTDOWNGUARD) &&
 		     (tmr->type != SCTP_TIMER_TYPE_ASOCKILL))
 			) {
 			SCTP_INP_DECR_REF(inp);
