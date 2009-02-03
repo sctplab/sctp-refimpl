ead519966da5c6b317fd501606001b6e5ce5a865
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-02-03 20:29:54 +0000

    id sync and fix the locking issue

--- trunk/KERN/netinet/sctp_crc32.c
+++ trunk/KERN/netinet/sctp_crc32.c
@@ -33,7 +33,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_crc32.c 184027 2008-10-18 15:53:31Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_crc32.c 188067 2009-02-03 11:04:03Z rrs $");
 #endif
 
 #include <sys/types.h>
