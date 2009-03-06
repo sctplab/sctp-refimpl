f233a9f54c8cc4296b91cd261246279c960940ba
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-03-06 11:05:21 +0000

    id sync.

--- trunk/KERN/netinet/sctp_indata.c
+++ trunk/KERN/netinet/sctp_indata.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_indata.c 189371 2009-03-04 20:54:42Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_indata.c 189444 2009-03-06 11:03:52Z rrs $");
 #endif
 
 #include <netinet/sctp_os.h>
