29ce699ddf6bba5a304779a8eebb706539f02c82
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-03-28 12:05:20 +0000

    more trys

--- APPS/atomic_performance/test_atomic.c
+++ APPS/atomic_performance/test_atomic.c
@@ -45,34 +45,17 @@
 void
 add_one_atomic(uint32_t *val)
 {
-	atomic_add_int(val, 1);
+	uint32_t i;
+	for (i=0; i<3000000000; i++) {
+		atomic_add_int(val, 1);
+	}
 }
 
 void
 add_one(uint32_t *val)
 {
-	*val += 1;
-}
-
-
-int
-main (int argc, char **argv)
-{
-	int number, i;
-	uint32_t val1=0, val2=0;
-	if (argc < 2) {
-		printf("Sorry use %s num-of-times\n", argv[0]);
-		return (-1);
+	uint32_t i;
+	for (i=0; i<3000000000; i++) {
+		*val += 1;
 	}
-	number = strtol(argv[1], NULL, 0);
-	for (i=0; i<number; i++) {
-		add_one(&val1);
-		add_one_atomic(&val2);
-	}
-	printf("Val 1 is %u Val 2 is %u\n", val1, val2);
-	return (0);
-
 }
-
-
-
