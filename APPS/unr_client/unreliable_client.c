40738bd19e7fae81f0d57f5fe222b56fe751cbfa
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-04-02 12:48:28 +0000

    more fun.

--- APPS/unr_client/unreliable_client.c
+++ APPS/unr_client/unreliable_client.c
@@ -17,24 +17,33 @@
 #define DISCARD_PORT 30002
 int fd;
 
+int iframe_rel=0;
+int pframe_rel=0;
+int bframe_rel=0;
+
 struct sockaddr_in addr;
 char buffer[SIZE_OF_MESSAGE];
-
+int icount=0, bcount=0, pcount=0;
 int
 send_iframe()
 {
 	int ret;
+	buffer[0] = 'I';
 	if ((ret = sctp_sendmsg(fd,
 			 (const void *)buffer, SIZE_OF_I_FRAME,
 			 (struct sockaddr *)&addr, sizeof(struct sockaddr_in),
 			 htonl(73),       /* PPID */
 			 SCTP_PR_SCTP_RTX, /* flags */
 			 1,                /* stream identifier */
-			 0,                /* Max number of rtx */
+			 iframe_rel,                /* Max number of rtx */
 			 0                 /* context */
 		     )) < 0) {
 		perror("sctp_sendmsg - I");
+	} else {
+		icount++;
 	}
+	printf("I:%d\r", icount);
+	fflush(stdout);
 	return(ret);
 }
 
@@ -42,17 +51,22 @@ int
 send_pframe()
 {
 	int ret;
+	buffer[0] = 'P';
 	if ((ret = sctp_sendmsg(fd,
 			 (const void *)buffer, SIZE_OF_P_FRAME,
 			 (struct sockaddr *)&addr, sizeof(struct sockaddr_in),
 			 htonl(80),       /* PPID */
 			 SCTP_PR_SCTP_RTX, /* flags */
 			 1,                /* stream identifier */
-			 0,                /* Max number of rtx */
+			 pframe_rel,                /* Max number of rtx */
 			 0                 /* context */
 		     )) < 0) {
 		perror("sctp_sendmsg - P");
+	} else {
+		pcount++;
 	}
+	printf("I:%d P:%d\r", icount, pcount);
+	fflush(stdout);
 	return(ret);
 }
 
@@ -60,17 +74,22 @@ int
 send_bframe()
 {
 	int ret;
+	buffer[0] = 'B';
 	if ((ret = sctp_sendmsg(fd,
 			 (const void *)buffer, SIZE_OF_P_FRAME,
 			 (struct sockaddr *)&addr, sizeof(struct sockaddr_in),
 			 htonl(66),       /* PPID */
 			 SCTP_PR_SCTP_RTX, /* flags */
 			 1,                /* stream identifier */
-			 0,                /* Max number of rtx */
+			 bframe_rel,                /* Max number of rtx */
 			 0                 /* context */
 		     )) < 0) {
 		perror("sctp_sendmsg - B");
+	} else {
+		bcount++;
 	}
+	printf("I:%d P:%d B:%d\r", icount, pcount, bcount);
+	fflush(stdout);
 	return(ret);
 }
 
@@ -86,9 +105,20 @@ int main(int argc, char **argv)
 	char *toaddr = NULL;
 	int port = htons(DISCARD_PORT);
 
-	while((i= getopt(argc,argv,"b:h:m:?")) != EOF)
+	while((i= getopt(argc,argv,"P:I:B:b:h:m:?")) != EOF)
 	{
 		switch(i) {
+		case 'P':
+			pframe_rel = strtol(optarg, NULL, 0);
+			break;
+		case 'I':
+			iframe_rel = strtol(optarg, NULL, 0);
+			break;
+		case 'B':
+			bframe_rel = strtol(optarg, NULL, 0);
+			break;
+
+
 		case 'b':
 			if (bndcnt < 10) {
 				baddr[bndcnt] = optarg;
@@ -111,6 +141,7 @@ int main(int argc, char **argv)
 	use:
 		printf("use %s -h to-addr-in-dot-notation [-m message-cnt -b bindaddr]\n",
 		       argv[0]);
+		printf("                    [-P rel -I rel -B rel]\n");
 		exit(0);
 	}
 	memset((void *)buffer, 'A', SIZE_OF_MESSAGE);
@@ -159,6 +190,6 @@ int main(int argc, char **argv)
 	}
 	if (close(fd) < 0)
 		perror("close");
-	
+	printf("\n");
 	return(0);
 }
