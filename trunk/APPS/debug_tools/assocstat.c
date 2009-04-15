807e04131a04d3f9970f07a89712fcab54e4a19c
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-04-15 09:59:01 +0000

    Add license.

--- trunk/APPS/debug_tools/assocstat.c
+++ trunk/APPS/debug_tools/assocstat.c
@@ -1,3 +1,34 @@
+/*-
+ * Copyright (c) 2009, by Michael Tuexen and Randall Stewart.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * a) Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ * b) Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the distribution.
+ *
+ * c) Neither the name of the authors nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -27,24 +58,26 @@ int main(int argc, char *argv[])
 	int no_stats=0;
 	union sctp_sockstore primary_addr;
 	int is_primary_addr;
+
 	if (argc > 1) {
 		if (strncmp(argv[1], "-n", 2) == 0) {
 			no_stats = 1;
 		}
 	}
+
 	if (sysctlbyname("net.inet.sctp.stats", &stat, &len, NULL, 0) < 0) {
 		printf("Error %d (%s) could not get the stat\n", errno, strerror(errno));
 		return(0);
 	}
 
-#define	p(f, n)								\
+#define p(f, n)                                                        \
 	printf("%-10.10s=%08lX%s", n, stat.f, ((cnt++&3)==3)?"\n":" ")
-#define nl(n)							\
-	do {							\
-		printf("%s%s\n", ((cnt&3)==0)?"":"\n", n);	\
-		cnt = 0;					\
+#define nl(n)                                                          \
+	do {                                                           \
+		printf("%s%s\n", ((cnt&3)==0)?"":"\n", n);             \
+		cnt = 0;                                               \
 	} while(0)
-	
+
 	if (no_stats == 0) {
 		nl("MIB");
 		p(sctps_currestab,           "currentestab");
