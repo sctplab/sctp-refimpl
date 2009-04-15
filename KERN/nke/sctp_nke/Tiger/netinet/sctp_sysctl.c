033cc8da4fe4d00fc90a1220bf6e92d5221ccec1
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-04-15 13:26:45 +0000

    Sync with main tree.

--- KERN/nke/sctp_nke/Tiger/netinet/sctp_sysctl.c
+++ KERN/nke/sctp_nke/Tiger/netinet/sctp_sysctl.c
@@ -583,7 +583,13 @@ sctp_assoclist(SYSCTL_HANDLER_ARGS)
 				xraddr.cwnd = net->cwnd;
 				xraddr.flight_size = net->flight_size;
 				xraddr.mtu = net->mtu;
+#if defined(__FreeBSD__)
+#if __FreeBSD_version >= 800000
 				xraddr.rtt = net->rtt;
+#endif
+#else
+				xraddr.rtt = net->rtt;
+#endif
 				xraddr.start_time.tv_sec = (uint32_t)net->start_time.tv_sec;
 				xraddr.start_time.tv_usec = (uint32_t)net->start_time.tv_usec;
 				SCTP_INP_RUNLOCK(inp);
