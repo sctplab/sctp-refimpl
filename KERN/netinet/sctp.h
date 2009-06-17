163bd324ef0ea414c24353ceaf49c3e4d5cef131
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-06-17 12:37:15 +0000

    Sync to BSD Id's. First round of NR-Sack changes are now complete..

--- KERN/netinet/sctp.h
+++ KERN/netinet/sctp.h
@@ -31,7 +31,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 189790 2009-03-14 13:42:13Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 194355 2009-06-17 12:34:56Z rrs $");
 
 #endif
 #ifndef _NETINET_SCTP_H_
