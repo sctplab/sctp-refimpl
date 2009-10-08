8d1be11f2a8b073e5cb2647a6509cc4aa6b9ad8e
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-10-08 11:42:01 +0000

    fix so that id's are in sync.

--- trunk/KERN/netinet/sctp_output.c
+++ trunk/KERN/netinet/sctp_output.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 197288 2009-09-17 15:11:12Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 197856 2009-10-08 11:36:06Z rrs $");
 #endif
 
 #include <netinet/sctp_os.h>
