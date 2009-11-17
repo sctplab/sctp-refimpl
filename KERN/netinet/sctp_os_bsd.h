e09da3193867835abf2ad3d7225f0e22b89beb7a
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-11-17 20:59:08 +0000

    Sync FBSDIDs.

--- KERN/netinet/sctp_os_bsd.h
+++ KERN/netinet/sctp_os_bsd.h
@@ -29,7 +29,7 @@
  */
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_os_bsd.h 199370 2009-11-17 13:08:15Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_os_bsd.h 199437 2009-11-17 20:56:14Z tuexen $");
 #endif
 #ifndef __sctp_os_bsd_h__
 #define __sctp_os_bsd_h__
