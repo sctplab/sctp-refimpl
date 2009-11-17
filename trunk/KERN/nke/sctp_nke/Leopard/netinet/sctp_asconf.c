2c1e9e166307d2b1346a1b0b79e27310a1e56034
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-11-17 13:22:01 +0000

    Sync with main tree.

--- trunk/KERN/nke/sctp_nke/Leopard/netinet/sctp_asconf.c
+++ trunk/KERN/nke/sctp_nke/Leopard/netinet/sctp_asconf.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_asconf.c 197288 2009-09-17 15:11:12Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_asconf.c 199372 2009-11-17 13:11:23Z tuexen $");
 #endif
 #include <netinet/sctp_os.h>
 #include <netinet/sctp_var.h>
