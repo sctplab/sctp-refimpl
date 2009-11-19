52e3c8c320d6a72edf1275f1a0f473f90f58c6e5
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2009-11-19 15:42:31 +0000

    Sync with main tree.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_pcb.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_pcb.c
@@ -6083,10 +6083,8 @@ sctp_pcb_finish(void)
 #endif
 #if defined(__Windows__)
 	if (SCTP_BASE_INFO(iterator_thread_obj) != NULL) {
-		KIRQL oldIrql;
 		NTSTATUS status = STATUS_SUCCESS;
 
-		KeLowerIrql(PASSIVE_LEVEL);
 		KeSetEvent(&SCTP_BASE_INFO(iterator_wakeup[1]), IO_NO_INCREMENT, FALSE);
 		status = KeWaitForSingleObject(SCTP_BASE_INFO(iterator_thread_obj),
 					       Executive,
@@ -6094,8 +6092,6 @@ sctp_pcb_finish(void)
 					       FALSE,
 					       NULL);
 		ObDereferenceObject(SCTP_BASE_INFO(iterator_thread_obj));
-
-		KeRaiseIrql(DISPATCH_LEVEL, &oldIrql);
 	}
 #endif
 #if defined(__FreeBSD__)
@@ -7179,6 +7175,9 @@ sctp_drain()
 #if defined(__FreeBSD__) && __FreeBSD_version >= 801000
 	VNET_ITERATOR_DECL(vnet_iter);
 #else
+	struct sctp_inpcb *inp;
+	struct sctp_tcb *stcb;
+
 	SCTP_STAT_INCR(sctps_protocol_drain_calls);
 	if (SCTP_BASE_SYSCTL(sctp_do_drain) == 0) {
 		return;
@@ -7188,9 +7187,9 @@ sctp_drain()
 	VNET_LIST_RLOCK_NOSLEEP();
 	VNET_FOREACH(vnet_iter) {
 		CURVNET_SET(vnet_iter);
-#endif
 		struct sctp_inpcb *inp;
 		struct sctp_tcb *stcb;
+#endif
 
 #if defined(__FreeBSD__) && __FreeBSD_version >= 801000
 		SCTP_STAT_INCR(sctps_protocol_drain_calls);
