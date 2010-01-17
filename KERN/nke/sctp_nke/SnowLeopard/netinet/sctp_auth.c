050e18441504e455f623b8f1ed7c3a2014261044
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-01-17 20:22:07 +0000

    Copy over more changes from the main repo. Trees are NOT yet in sync.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_auth.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_auth.c
@@ -30,7 +30,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_auth.c 199437 2009-11-17 20:56:14Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_auth.c 202449 2010-01-16 20:04:17Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
