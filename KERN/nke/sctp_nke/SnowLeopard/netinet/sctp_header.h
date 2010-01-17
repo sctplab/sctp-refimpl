e1c4111f14045bad50a5ef1104a5c6afa9f44815
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-01-17 21:08:35 +0000

    Sync with main tree. Now they are in sync.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_header.h
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_header.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_header.h 188854 2009-02-20 15:03:54Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_header.h 202526 2010-01-17 21:00:28Z tuexen $");
 #endif
 
 #ifndef __sctp_header_h__
