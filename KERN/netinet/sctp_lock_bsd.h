61f4127a0de8e1fae696208fef122c97e6b0dad5
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-05-16 17:31:35 +0000

    white space changes

--- KERN/netinet/sctp_lock_bsd.h
+++ KERN/netinet/sctp_lock_bsd.h
@@ -3,30 +3,30 @@
 /*-
  * Copyright (c) 2001-2007, by Cisco Systems, Inc. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
- * a) Redistributions of source code must retain the above copyright notice, 
+ *
+ * a) Redistributions of source code must retain the above copyright notice,
  *   this list of conditions and the following disclaimer.
  *
- * b) Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
+ * b) Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
  *   the documentation and/or other materials provided with the distribution.
  *
- * c) Neither the name of Cisco Systems, Inc. nor the names of its 
- *    contributors may be used to endorse or promote products derived 
+ * c) Neither the name of Cisco Systems, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -79,7 +79,7 @@ extern int sctp_logoff_stuff;
 
 #define SCTP_IPI_COUNT_INIT()
 
-#define SCTP_STATLOG_INIT_LOCK() 
+#define SCTP_STATLOG_INIT_LOCK()
 #define SCTP_STATLOG_LOCK()
 #define SCTP_STATLOG_UNLOCK()
 #define SCTP_STATLOG_DESTROY()
@@ -90,14 +90,14 @@ extern int sctp_logoff_stuff;
              mtx_unlock(&SCTP_BASE_INFO(ipi_ep_mtx)); \
         } \
         mtx_destroy(SCTP_BASE_INFO(ipi_ep_mtx)); \
-      }  while (0) 
+      }  while (0)
 #else
 #define SCTP_INP_INFO_LOCK_DESTROY() do { \
         if(rw_wowned(&SCTP_BASE_INFO(ipi_ep_mtx))) { \
              rw_wunlock(&SCTP_BASE_INFO(ipi_ep_mtx)); \
         } \
         rw_destroy(&SCTP_BASE_INFO(ipi_ep_mtx)); \
-      }  while (0) 
+      }  while (0)
 #endif
 
 #if __FreeBSD_version <= 602000
@@ -149,7 +149,7 @@ extern int sctp_logoff_stuff;
              mtx_unlock(&SCTP_BASE_INFO(ipi_addr_mtx)); \
         } \
 	    mtx_destroy(&SCTP_BASE_INFO(ipi_addr_mtx)); \
-      }  while (0) 
+      }  while (0)
 
 
 #define SCTP_IPI_ADDR_RLOCK()	do { 					\
@@ -170,7 +170,7 @@ extern int sctp_logoff_stuff;
              rw_wunlock(&SCTP_BASE_INFO(ipi_addr_mtx)); \
         } \
 	rw_destroy(&SCTP_BASE_INFO(ipi_addr_mtx)); \
-      }  while (0) 
+      }  while (0)
 #define SCTP_IPI_ADDR_RLOCK()	do { 					\
              rw_rlock(&SCTP_BASE_INFO(ipi_addr_mtx));                         \
 } while (0)
@@ -381,7 +381,7 @@ extern int sctp_logoff_stuff;
              mtx_unlock(&SCTP_BASE_INFO(wq_addr_mtx)); \
         } \
 	    mtx_destroy(&SCTP_BASE_INFO(wq_addr_mtx)); \
-      }  while (0) 
+      }  while (0)
 
 #define SCTP_WQ_ADDR_LOCK()	do { \
              mtx_lock(&SCTP_BASE_INFO(wq_addr_mtx));  \
@@ -449,7 +449,7 @@ extern int sctp_logoff_stuff;
                        if(SCTP_BASE_INFO(ipi_count_chunk) != 0) \
   	               atomic_subtract_int(&SCTP_BASE_INFO(ipi_count_chunk), 1); \
 	        } while (0)
-#endif 
+#endif
 #define SCTP_INCR_READQ_COUNT() \
                 do { \
 		       atomic_add_int(&SCTP_BASE_INFO(ipi_count_readq),1); \
