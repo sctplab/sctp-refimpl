6bdcf972caba174d5d2365072638706c754f633a
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-06 16:13:16 +0000

    id sync

--- KERN/netinet/sctputil.c
+++ KERN/netinet/sctputil.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 208857 2010-06-05 21:39:52Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 208874 2010-06-06 16:07:40Z rrs $");
 #endif
 
 #include <netinet/sctp_os.h>
