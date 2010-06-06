f7491f6df45da5bf97ab167eaeccac70689079f9
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-06 16:00:24 +0000

    Sync with main tree.

--- trunk/KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_input.c
+++ trunk/KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_input.c
@@ -4538,7 +4538,8 @@ sctp_process_control(struct mbuf *m, int iphlen, int *offset, int length,
  				if ((stcb) && (stcb->asoc.total_output_queue_size)) {
 					;
 				} else {
-					if (locked_tcb) {
+					if (locked_tcb != stcb) {
+						/* Very unlikely */
 						SCTP_TCB_UNLOCK(locked_tcb);
 					}
 					*offset = length;
@@ -5418,6 +5419,12 @@ sctp_validate_no_locks(struct sctp_inpcb *inp)
 		if (mtx_owned(&lstcb->tcb_mtx)) {
 			panic("Own lock on stcb at return from input");
 		}
+	}
+	if (mtx_owned(&inp->inp_create_mtx)) {
+		panic("Own create lock on inp");
+	}
+	if (mtx_owned(&inp->inp_mtx)) {
+		panic("Own inp lock on inp");
 	}
 #endif
 }
