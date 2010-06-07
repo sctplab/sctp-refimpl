fbcbdb5e37110cbb708dbde215bd11f25f5fffd4
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-07 13:07:48 +0000

    id sync

--- trunk/KERN/netinet/sctp_pcb.c
+++ trunk/KERN/netinet/sctp_pcb.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 208883 2010-06-07 04:01:38Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 208891 2010-06-07 11:33:20Z rrs $");
 #endif
 
 #include <netinet/sctp_os.h>
