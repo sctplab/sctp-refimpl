c55b0652b90c11ca7229d0a160872bf915af2faa
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-11 11:39:41 +0000

    Get Mac OS X version compiling again. Not sure it is the right definition of SCTP_OS_TIMER_STOP_DRAIN.

--- trunk/KERN/netinet/sctp_callout.h
+++ trunk/KERN/netinet/sctp_callout.h
@@ -72,6 +72,8 @@ int sctp_os_timer_stop(sctp_os_timer_t *);
 #define SCTP_OS_TIMER_INIT	sctp_os_timer_init
 #define SCTP_OS_TIMER_START	sctp_os_timer_start
 #define SCTP_OS_TIMER_STOP	sctp_os_timer_stop
+/* MT FIXME: Is the following correct? */
+#define SCTP_OS_TIMER_STOP_DRAIN SCTP_OS_TIMER_STOP
 #define	SCTP_OS_TIMER_PENDING(tmr) ((tmr)->c_flags & SCTP_CALLOUT_PENDING)
 #define	SCTP_OS_TIMER_ACTIVE(tmr) ((tmr)->c_flags & SCTP_CALLOUT_ACTIVE)
 #define	SCTP_OS_TIMER_DEACTIVATE(tmr) ((tmr)->c_flags &= ~SCTP_CALLOUT_ACTIVE)
