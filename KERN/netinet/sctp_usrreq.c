797f93e5c25ba7b5fc51f09d25888dc1f757ac92
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-11 03:55:50 +0000

    id sync.

--- KERN/netinet/sctp_usrreq.c
+++ KERN/netinet/sctp_usrreq.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_usrreq.c 208863 2010-06-06 02:32:20Z rrs $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_usrreq.c 209029 2010-06-11 03:54:00Z rrs $");
 #endif
 #include <netinet/sctp_os.h>
 #ifdef __FreeBSD__
