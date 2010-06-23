163faef2cc8848177c3ec59850316ce6d0639d02
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-23 15:20:29 +0000

    Sync with main sources.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_pcb.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_pcb.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 209178 2010-06-14 21:25:07Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 209470 2010-06-23 15:19:07Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
