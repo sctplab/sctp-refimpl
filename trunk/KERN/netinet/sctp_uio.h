51c9d91ea0c8be1d196a65ceed2d2543de2ebaa3
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-06-24 12:52:49 +0000

    For us cranky old folks, add ntohll :-)

--- trunk/KERN/netinet/sctp_uio.h
+++ trunk/KERN/netinet/sctp_uio.h
@@ -975,6 +975,21 @@ union sctp_sockstore {
 	struct sockaddr sa;
 };
 
+
+/***********************************/
+/* And something for us old timers */
+#ifndef ntohll
+#include <sys/endian.h>
+#define ntohll(x) be64toh(x)
+#endif
+
+#ifndef htonll
+#include <sys/endian.h>
+#define htonll(x) htobe64(x)
+#endif
+/***********************************/
+
+
 struct xsctp_inpcb {
 	uint32_t last;
 	uint32_t flags;
