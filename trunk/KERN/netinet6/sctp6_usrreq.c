053225cf8a36caa47d221f7b54f9d9f7811e0831
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-09-15 20:42:34 +0000

    Sync FBSDID.

--- trunk/KERN/netinet6/sctp6_usrreq.c
+++ trunk/KERN/netinet6/sctp6_usrreq.c
@@ -31,7 +31,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet6/sctp6_usrreq.c 211969 2010-08-29 18:50:30Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet6/sctp6_usrreq.c 212699 2010-09-15 20:41:20Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
