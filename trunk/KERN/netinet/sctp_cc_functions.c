f2a77a0db9749668025d1e8e978cc2aae4c6ccfb
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-09-15 20:50:06 +0000

    Use TAILQ_EMPTY() for testing. Remove assignment without effect.

--- trunk/KERN/netinet/sctp_cc_functions.c
+++ trunk/KERN/netinet/sctp_cc_functions.c
@@ -171,9 +171,9 @@ sctp_cwnd_update_after_sack(struct sctp_tcb *stcb,
 			 * So, first of all do we need to have a Early FR
 			 * timer running?
 			 */
-			if (((TAILQ_FIRST(&asoc->sent_queue)) &&
-			    (net->ref_count > 1) &&
-			    (net->flight_size < net->cwnd)) ||
+			if ((!TAILQ_EMPTY(&asoc->sent_queue) &&
+			     (net->ref_count > 1) &&
+			     (net->flight_size < net->cwnd)) ||
 			    (reneged_all)) {
 				/*
 				 * yes, so in this case stop it if its
@@ -653,7 +653,6 @@ sctp_hs_cwnd_decrease(struct sctp_tcb *stcb, struct sctp_nets *net)
 	int old_cwnd = net->cwnd;
 
 	cur_val = net->cwnd >> 10;
-	indx = net->last_hs_used;
 	if (cur_val < sctp_cwnd_adjust[0].cwnd) {
 		/* normal mode */
 		net->ssthresh = net->cwnd / 2;
@@ -788,9 +787,9 @@ sctp_hs_cwnd_update_after_sack(struct sctp_tcb *stcb,
 			 * So, first of all do we need to have a Early FR
 			 * timer running?
 			 */
-			if (((TAILQ_FIRST(&asoc->sent_queue)) &&
-			    (net->ref_count > 1) &&
-			    (net->flight_size < net->cwnd)) ||
+			if ((!TAILQ_EMPTY(&asoc->sent_queue) &&
+			     (net->ref_count > 1) &&
+			     (net->flight_size < net->cwnd)) ||
 			    (reneged_all)) {
 				/*
 				 * yes, so in this case stop it if its
@@ -1270,9 +1269,9 @@ sctp_htcp_cwnd_update_after_sack(struct sctp_tcb *stcb,
 			 * So, first of all do we need to have a Early FR
 			 * timer running?
 			 */
-			if (((TAILQ_FIRST(&asoc->sent_queue)) &&
-			    (net->ref_count > 1) &&
-			    (net->flight_size < net->cwnd)) ||
+			if ((!TAILQ_EMPTY(&asoc->sent_queue) &&
+			     (net->ref_count > 1) &&
+			     (net->flight_size < net->cwnd)) ||
 			    (reneged_all)) {
 				/*
 				 * yes, so in this case stop it if its
