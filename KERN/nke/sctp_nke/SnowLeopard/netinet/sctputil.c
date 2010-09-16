6081b9851812c20e024fdd5b87ca62e03610233f
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-09-16 00:01:49 +0000

    Sync with main sources.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctputil.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctputil.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 212713 2010-09-15 23:40:36Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 212714 2010-09-15 23:56:25Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
@@ -1016,10 +1016,6 @@ sctp_init_asoc(struct sctp_inpcb *m, struct sctp_tcb *stcb,
 	asoc->peers_rwnd = SCTP_SB_LIMIT_RCV(m->sctp_socket);
 
 	asoc->smallest_mtu = m->sctp_frag_point;
-#ifdef SCTP_PRINT_FOR_B_AND_M
-	SCTP_PRINTF("smallest_mtu init'd with asoc to :%d\n",
-		    asoc->smallest_mtu);
-#endif
 	asoc->minrto = m->sctp_ep.sctp_minrto;
 	asoc->maxrto = m->sctp_ep.sctp_maxrto;
 
@@ -2533,10 +2529,6 @@ sctp_mtu_size_reset(struct sctp_inpcb *inp,
 	struct sctp_tmit_chunk *chk;
 	unsigned int eff_mtu, ovh;
 
-#ifdef SCTP_PRINT_FOR_B_AND_M
-	SCTP_PRINTF("sctp_mtu_size_reset(%p, asoc:%p mtu:%d\n",
-		    inp, asoc, mtu);
-#endif
 	asoc->smallest_mtu = mtu;
 	if (inp->sctp_flags & SCTP_PCB_FLAGS_BOUND_V6) {
 		ovh = SCTP_MIN_OVERHEAD;
