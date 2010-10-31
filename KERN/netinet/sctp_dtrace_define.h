42ca8913ce00a9be94171c8e303a04737158491c
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-10-31 13:08:13 +0000

    update the list and fix next syntax (no dtrae yet defined)

--- KERN/netinet/sctp_dtrace_define.h
+++ KERN/netinet/sctp_dtrace_define.h
@@ -11,63 +11,63 @@ SDT_PROVIDER_DEFINE(sctp);
 /********************************************************/
 SDT_PROBE_DEFINE(sctp, cwnd, net, val, val);
 /* The Vtag for this end */
-SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, val, 0, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, 0, "uint32_t");
 /* The port number of the local side << 16 | port number of remote 
  * in network byte order.
  */
-SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, val, 1, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, 1, "uint32_t");
 /* The pointer to the struct sctp_nets * changing */
-SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, val, 2, "uintptr_t");
+SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, 2, "uintptr_t");
 /* The old value of the cwnd  */
-SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, val, 3, "int");
+SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, 3, "int");
 /* The new value of the cwnd */
-SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, val, 4, "int");
+SDT_PROBE_ARGTYPE(sctp, cwnd, net, val, 4, "int");
 
 /********************************************************/
 /* Rwnd probe - tracks changes in the receiver window for an assoc */
 /********************************************************/
 SDT_PROBE_DEFINE(sctp, rwnd, assoc, val, val);
 /* The Vtag for this end */
-SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, val, 0, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, 0, "uint32_t");
 /* The port number of the local side << 16 | port number of remote 
  * in network byte order.
  */
-SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, val, 1, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, 1, "uint32_t");
 /* The up/down amount */
-SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, val, 2, "int");
+SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, 2, "int");
 /* The new value of the cwnd */
-SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, val, 3, "int");
+SDT_PROBE_ARGTYPE(sctp, rwnd, assoc, val, 3, "int");
 
 /********************************************************/
 /* flight probe - tracks changes in the flight size on a net or assoc */
 /********************************************************/
 SDT_PROBE_DEFINE(sctp, flightsize, net, val, val);
 /* The Vtag for this end */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, val, 0, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, 0, "uint32_t");
 /* The port number of the local side << 16 | port number of remote 
  * in network byte order.
  */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, val, 1, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, 1, "uint32_t");
 /* The pointer to the struct sctp_nets * changing */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, val, 2, "uintptr_t");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, 2, "uintptr_t");
 /* The up/down amount */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, val, 3, "int");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, 3, "int");
 /* The new value of the cwnd */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, val, 4, "int");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, val, 4, "int");
 /********************************************************/
 /* The total flight version */
 /********************************************************/
 SDT_PROBE_DEFINE(sctp, flightsize, assoc, val, val);
 /* The Vtag for this end */
-SDT_PROBE_ARGTYPE(sctp, flightsize, assoc, val, val, 0, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, flightsize, assoc, val, 0, "uint32_t");
 /* The port number of the local side << 16 | port number of remote 
  * in network byte order.
  */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, assoc, val, 1, "uint32_t");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, assoc, 1, "uint32_t");
 /* The up/down amount */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, assoc, val, 2, "int");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, assoc, 2, "int");
 /* The new value of the cwnd */
-SDT_PROBE_ARGTYPE(sctp, flightsize, net, assoc, val, 3, "int");
+SDT_PROBE_ARGTYPE(sctp, flightsize, net, assoc, 3, "int");
 
 #endif
 #endif
