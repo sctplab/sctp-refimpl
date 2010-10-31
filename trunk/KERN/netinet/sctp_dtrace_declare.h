ea2ece77d3707da8e3121b2f9fed3e28de6da529
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-10-31 11:33:16 +0000

    Add a few probes for Dtrace.

---
+++ trunk/KERN/netinet/sctp_dtrace_declare.h
@@ -0,0 +1,30 @@
+#ifndef __sctp_dtrace_declare_h__
+#if defined(__FreeBSD__)
+#include "opt_kdtrace.h"
+#include <sys/kernel.h>
+#include <sys/sdt.h>
+
+/* Declare the SCTP provider */
+SDT_PROVIDER_DECLARE(sctp);
+
+/* The probes we have so far: */
+
+/* One to track a net's cwnd */
+SDT_PROBE_DECLARE(sctp, cwnd, net, val, val);
+
+/* One to track an associations rwnd */
+SDT_PROBE_DECLARE(sctp, rwnd, assoc, val, val);
+
+/* One to track a net's flight size */ 
+SDT_PROBE_DECLARE(sctp, flightsize, net, val, val);
+
+/* One to track an associations flight size */ 
+SDT_PROBE_DECLARE(sctp, flightsize, assoc, val, val);
+
+
+
+
+
+
+#endif
+#endif
