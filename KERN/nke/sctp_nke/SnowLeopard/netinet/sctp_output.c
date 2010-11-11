5c710ef1a80410aa9b44980802c2c0e5ffbda2d1
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-11-11 18:51:03 +0000

    Sync with main tree.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_output.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_output.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 214876 2010-11-06 13:30:54Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 215134 2010-11-11 18:41:03Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
@@ -10296,18 +10296,17 @@ sctp_send_sack(struct sctp_tcb *stcb)
 		}
 	}
 
-	if (((type == SCTP_SELECTIVE_ACK) &&
-	     (((asoc->mapping_array[0]|asoc->nr_mapping_array[0]) & 0x01) == 0x00)) ||
-	    ((type == SCTP_NR_SELECTIVE_ACK) &&
-	     ((asoc->mapping_array[0] & 0x01) == 0x00))) {
-		sel_start = 0;
-	} else {
-		sel_start = 1;
-	}
 	if (compare_with_wrap(asoc->mapping_array_base_tsn, asoc->cumulative_tsn, MAX_TSN)) {
 		offset = 1;
 	} else {
 		offset = asoc->mapping_array_base_tsn - asoc->cumulative_tsn;
+	}
+	if ((offset == 1) ||
+	    ((type == SCTP_NR_SELECTIVE_ACK) &&
+	     ((asoc->mapping_array[0] & (1 << (1 - offset))) == 0))) {
+		sel_start = 0;
+	} else {
+		sel_start = 1;
 	}
 	if (((type == SCTP_SELECTIVE_ACK) &&
 	     compare_with_wrap(highest_tsn, asoc->cumulative_tsn, MAX_TSN)) ||
@@ -10378,7 +10377,7 @@ sctp_send_sack(struct sctp_tcb *stcb)
 			siz = (((MAX_TSN - asoc->mapping_array_base_tsn) + 1) + asoc->highest_tsn_inside_nr_map + 7) / 8;
 		}
 
-		if ((asoc->nr_mapping_array[0] & 0x01) == 0x00) {
+		if ((asoc->nr_mapping_array[0] & (1 << (1 - offset))) == 0) {
 			sel_start = 0;
 		} else {
 			sel_start = 1;
