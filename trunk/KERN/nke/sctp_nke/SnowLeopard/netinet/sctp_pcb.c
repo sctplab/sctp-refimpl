70ac4eb9b0290582a568762eabc33e256fa20bcd
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-11-13 12:54:47 +0000

    Sync with main tree.

--- trunk/KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_pcb.c
+++ trunk/KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_pcb.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 215039 2010-11-09 16:18:32Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 215241 2010-11-13 12:52:44Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
