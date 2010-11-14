cd4b5a299dae61cc06c44fbe349ef32922e53b12
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-11-14 16:41:30 +0000

    Take out special code for CRC handling when using loopback and IPv6. It will be handled by the loopback interface (shortly).

--- KERN/netinet/sctp_output.c
+++ KERN/netinet/sctp_output.c
@@ -4208,20 +4208,20 @@ sctp_lowlevel_chunk_output(struct sctp_inpcb *inp,
 #if defined(SCTP_WITH_NO_CSUM)
 			SCTP_STAT_INCR(sctps_sendnocrc);
 #else
+#if defined(__FreeBSD__) && __FreeBSD_version >= 800000
+			m->m_pkthdr.csum_flags = CSUM_SCTP;
+			m->m_pkthdr.csum_data = 0;
+			SCTP_STAT_INCR(sctps_sendhwcrc);
+#else
 			if (!(SCTP_BASE_SYSCTL(sctp_no_csum_on_loopback) &&
 			     (stcb) &&
 			     (stcb->asoc.loopback_scope))) {
-#if defined(__FreeBSD__) && __FreeBSD_version >= 800000
-				m->m_pkthdr.csum_flags = CSUM_SCTP;
-				m->m_pkthdr.csum_data = 0;
-				SCTP_STAT_INCR(sctps_sendhwcrc);
-#else
 				sctphdr->checksum = sctp_calculate_cksum(m, sizeof(struct ip6_hdr));
 				SCTP_STAT_INCR(sctps_sendswcrc);
-#endif
 			} else {
 				SCTP_STAT_INCR(sctps_sendnocrc);
 			}
+#endif
 #endif
 		}
 		/* send it out. table id is taken from stcb */
