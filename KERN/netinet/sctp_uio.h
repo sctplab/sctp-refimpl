9e5e548048426fb2dc8b68e70834fdf027fe1e3b
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2010-11-25 13:45:38 +0000

    id sync

--- KERN/netinet/sctp_uio.h
+++ KERN/netinet/sctp_uio.h
@@ -31,7 +31,7 @@
 /* $KAME: sctp_uio.h,v 1.11 2005/03/06 16:04:18 itojun Exp $	 */
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_uio.h 215410 2010-11-16 22:16:38Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_uio.h 215817 2010-11-25 13:39:55Z rrs $");
 #endif
 
 #ifndef __sctp_uio_h__
