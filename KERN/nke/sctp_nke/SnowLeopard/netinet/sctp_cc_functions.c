b67295c4e5b98f94953e614056facd151db91a50
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-01-19 22:16:09 +0000

    Sync with main sources.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_cc_functions.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_cc_functions.c
@@ -44,7 +44,7 @@
 #include <netinet/sctp_dtrace_declare.h>
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_cc_functions.c 217469 2011-01-16 10:02:46Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_cc_functions.c 217611 2011-01-19 22:10:35Z tuexen $");
 #endif
 
 static void
