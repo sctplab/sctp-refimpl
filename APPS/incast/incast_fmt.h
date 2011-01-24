de32fb6e8b7495c812645bcbef4f1cf4799ffcd8
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-01-24 14:11:02 +0000

    update

--- APPS/incast/incast_fmt.h
+++ APPS/incast/incast_fmt.h
@@ -78,5 +78,5 @@ struct incast_control {
 
 int translate_ip_address(char *host, struct sockaddr_in *sa);
 void incast_run_clients(struct incast_control *ctrl);
-void print_an_address(struct sockaddr *a);
+void print_an_address(struct sockaddr *a, int cr);
 #endif
