e86a5f17bd032a916cf0fe84243514f6bcf36ecb
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-01 07:56:40 +0000

    What happens when prev_cwnd is larger than current?

--- trunk/KERN/netinet/sctp_cc_functions.c
+++ trunk/KERN/netinet/sctp_cc_functions.c
@@ -495,7 +495,14 @@ sctp_cwnd_update_after_ecn_echo(struct sctp_tcb *stcb, struct sctp_nets *net,
 			/* Go to CA with the cwnd at the point we sent
 			 * the TSN that was marked with a CE.
 			 */
-			net->cwnd = net->ecn_prev_cwnd - (net->mtu * num_pkt_lost);
+			if (net->ecn_prev_cwnd < net->cwnd) {
+				/* Restore to prev cwnd */
+				net->cwnd = net->ecn_prev_cwnd - (net->mtu * num_pkt_lost);
+			} else {
+				/* Just cut in 1/2 */
+				net->cwnd /= 2;
+			}
+			/* Drop to CA */
 			net->ssthresh = net->cwnd - (num_pkt_lost * net->mtu);
 		} else {
 			/* Further tuning down required over the drastic orginal cut */
