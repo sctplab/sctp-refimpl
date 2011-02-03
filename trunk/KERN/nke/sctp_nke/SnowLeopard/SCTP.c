62d96ce75462ba2cc340fa38aaed2b2e88a9d8a5
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-03 20:14:57 +0000

    Add support for fr-maxburst sysctl.

--- trunk/KERN/nke/sctp_nke/SnowLeopard/SCTP.c
+++ trunk/KERN/nke/sctp_nke/SnowLeopard/SCTP.c
@@ -71,6 +71,7 @@ extern struct sysctl_oid sysctl__net_inet_sctp_recvspace;
 extern struct sysctl_oid sysctl__net_inet_sctp_auto_asconf;
 #endif
 extern struct sysctl_oid sysctl__net_inet_sctp_ecn_enable;
+extern struct sysctl_oid sysctl__net_inet_sctp_fr_maxburst;
 extern struct sysctl_oid sysctl__net_inet_sctp_strict_sacks;
 #if !defined(SCTP_WITH_NO_CSUM)
 extern struct sysctl_oid sysctl__net_inet_sctp_loopback_nocsum;
@@ -353,6 +354,7 @@ SCTP_start (kmod_info_t * ki __attribute__((unused)), void * d __attribute__((un
 	sysctl_register_oid(&sysctl__net_inet_sctp_auto_asconf);
 #endif
 	sysctl_register_oid(&sysctl__net_inet_sctp_ecn_enable);
+	sysctl_register_oid(&sysctl__net_inet_sctp_fr_maxburst);
 	sysctl_register_oid(&sysctl__net_inet_sctp_strict_sacks);
 #if !defined(SCTP_WITH_NO_CSUM)
 	sysctl_register_oid(&sysctl__net_inet_sctp_loopback_nocsum);
@@ -479,6 +481,7 @@ SCTP_stop (kmod_info_t * ki __attribute__((unused)), void * d __attribute__((unu
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_auto_asconf);
 #endif
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_ecn_enable);
+	sysctl_unregister_oid(&sysctl__net_inet_sctp_fr_maxburst);
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_strict_sacks);
 #if !defined(SCTP_WITH_NO_CSUM)
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_loopback_nocsum);
