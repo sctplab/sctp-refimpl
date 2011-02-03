3bf93ddfd1ce69f7ba93b7912457318016791872
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-03 18:10:42 +0000

    Update Copyrights.

--- KERN/nke/sctp_nke/SnowLeopard/netinet/sctputil.c
+++ KERN/nke/sctp_nke/SnowLeopard/netinet/sctputil.c
@@ -1,5 +1,8 @@
 /*-
  * Copyright (c) 2001-2008, by Cisco Systems, Inc. All rights reserved.
+ * Copyright (c) 2008-2011, by Randall Stewart, rrs@lakerest.net and
+ *                          Michael Tuexen, tuexen@fh-muenster.de
+ *                          All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
