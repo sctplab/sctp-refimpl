e6c8130f1e7879d1b69516fa6949f6856fa2c8e9
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-04 12:15:13 +0000

    Massive copyright update, after critism (brucifying like), that they did not like Michael and I's style ;-)
    If I missed adding your name to a file, you are responsible ;-)

--- trunk/KERN/netinet/sctp_ss_functions.c
+++ trunk/KERN/netinet/sctp_ss_functions.c
@@ -1,7 +1,6 @@
 /*-
- * Copyright (c) 2010-2011, by Randall Stewart, rrs@lakerest.net and
- *                          Michael Tuexen, tuexen@fh-muenster.de
- *                          All rights reserved.
+ * Copyright (c) 2008-2011, by Michael Tuexen. All rights reserved.
+ * Copyright (c) 2008-2011, by Randall Stewart. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
