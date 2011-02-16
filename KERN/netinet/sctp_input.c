6c6802acf31fbd2de03c9d8a0e54842d9601e5a1
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-16 22:30:32 +0000

    Use appropriate ifdefs.

--- KERN/netinet/sctp_input.c
+++ KERN/netinet/sctp_input.c
@@ -2687,7 +2687,9 @@ sctp_handle_cookie_echo(struct mbuf *m, int iphlen, int offset,
 #endif
 			atomic_add_int(&(*stcb)->asoc.refcnt, 1);
 			SCTP_TCB_UNLOCK((*stcb));
+#if defined(__FreeBSD__) && __FreeBSD_version >= 801000
 			CURVNET_SET(oso->so_vnet);
+#endif
 #if defined(__APPLE__)
 			SCTP_SOCKET_LOCK(oso, 1);
 #endif
@@ -2705,7 +2707,9 @@ sctp_handle_cookie_echo(struct mbuf *m, int iphlen, int offset,
 #if defined(__APPLE__)
 			SCTP_SOCKET_UNLOCK(oso, 1);
 #endif
+#if defined(__FreeBSD__) && __FreeBSD_version >= 801000
 			CURVNET_RESTORE();
+#endif
 			SCTP_TCB_LOCK((*stcb));
 			atomic_subtract_int(&(*stcb)->asoc.refcnt, 1);
 
