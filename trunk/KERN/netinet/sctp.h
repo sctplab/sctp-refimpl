8ed5e41d9c581179c39f9a682ea7e7fa58910fd0
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-24 14:50:43 +0000

    code does NOT compile with the FreeBSDid in place.

--- trunk/KERN/netinet/sctp.h
+++ trunk/KERN/netinet/sctp.h
@@ -33,7 +33,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 218319 2011-02-05 12:12:51Z rrs $");
+/*__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 218319 2011-02-05 12:12:51Z rrs $");*/
 #endif
 
 #ifndef _NETINET_SCTP_H_
