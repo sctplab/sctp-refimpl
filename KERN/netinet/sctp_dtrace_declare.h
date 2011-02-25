96a757022e07cd487a2783eb0fd2e0a07eec2466
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-25 11:52:03 +0000

    my bad

--- KERN/netinet/sctp_dtrace_declare.h
+++ KERN/netinet/sctp_dtrace_declare.h
@@ -59,7 +59,7 @@ SDT_PROBE_DECLARE(sctp, cwnd, net, ecn);
 /* update at a Packet-Drop -- decrease */
 SDT_PROBE_DECLARE(sctp, cwnd, net, pd);
 /* Rttvar probe declaration */
-SDT_PROBE_DECLARE(sctp, cwnd, net, rttvar, rttvar);
+SDT_PROBE_DECLARE(sctp, cwnd, net, rttvar);
 
 /* One to track an associations rwnd */
 SDT_PROBE_DECLARE(sctp, rwnd, assoc, val);
