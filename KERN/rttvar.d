5e65d55ea60c3f5d379536d2564c02be01b5ac17
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-25 14:36:53 +0000

    update d script to match new format

--- KERN/rttvar.d
+++ KERN/rttvar.d
@@ -1,11 +1,12 @@
 
 sctp:cwnd:net:rttvar
 {
-	printf("%d %d %d %ld %ld %ld\n",
+	printf("%d %d %d %ld %ld %ld %ld\n",
 		   (uint16_t)((arg4 >> 16) & 0x000000000000ffff),
 		   (uint32_t)((arg4 >> 32) & 0x00000000ffffffff),
 	       	   (uint16_t)(arg4 & 0x000000000000ffff),
-		   arg1,
+		   ((arg1 >> 32) & 0x00000000ffffffff),
+		   (arg1 & 0x00000000ffffffff),
 		   arg2,
 		   arg3);
 }
