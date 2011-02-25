354f8a40390dac8bffac0f888d385e83e78d6544
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-02-25 13:35:41 +0000

    add a trial dtrace script

---
+++ KERN/rttvar.d
@@ -0,0 +1,11 @@
+
+sctp:cwnd:net:rttvar
+{
+	printf("%d %d %d %ld %ld %ld\n",
+		   (uint16_t)((arg4 >> 16) & 0x000000000000ffff),
+		   (uint32_t)((arg4 >> 32) & 0x00000000ffffffff),
+	       	   (uint16_t)(arg4 & 0x000000000000ffff),
+		   arg1,
+		   arg2,
+		   arg3);
+}
