79a635312a627033b98242555727c8ac1e18b009
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-03-01 14:39:32 +0000

    dtrace script update and new one

--- trunk/KERN/rttvar.d
+++ trunk/KERN/rttvar.d
@@ -1,8 +1,7 @@
-#pragma D option switchrate=10hz
 
 sctp:cwnd:net:rttvar
 {
-	printf("%d %d %d %ld %ld %ld %ld\n",
+	printf("%d %d %d %ld %ld %ld %ld",
 		   (uint16_t)((arg4 >> 16) & 0x000000000000ffff),
 		   (uint32_t)((arg4 >> 32) & 0x00000000ffffffff),
 	       	   (uint16_t)(arg4 & 0x000000000000ffff),
