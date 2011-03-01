1b707148c2794a0fe8b2726ba0e54fcf239cf669
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-03-01 14:58:06 +0000

    put lables on it (except first one that I plot).

--- KERN/rttstep.d
+++ KERN/rttstep.d
@@ -2,7 +2,7 @@
 sctp:cwnd:net:rttstep
 {
 	/* probepoint cwnd ret lbw nbw  bwrtt nrtt scwnd sc lstate*/ 
-	printf("%d %d %d %d %d %d %d %d %d %d ",
+	printf("%d cwnd:%d ret:%d lbw:%d nbw:%d bwrtt:%d nrtt:%d scwnd:%d stepc:%d lstate:%d ",
 		   (uint16_t)((arg4 >> 16) & 0x000000000000ffff),
 		   (uint32_t)((arg4 >> 32) & 0x00000000ffffffff),
 	       	   (uint16_t)(arg4 & 0x000000000000ffff),
