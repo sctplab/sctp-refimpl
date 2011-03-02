d018321c5162c7ce11faf3ef16e7e0e9658658ca
randall <randall@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-03-02 11:35:04 +0000

    add hooks to know when instant bw (this rtt) can be calculated.. and then only do a probe point on it for dtrace for now.

--- trunk/KERN/netinet/sctp_constants.h
+++ trunk/KERN/netinet/sctp_constants.h
@@ -385,8 +385,8 @@ __FBSDID("$FreeBSD: head/sys/netinet/sctp_constants.h 219120 2011-03-01 00:37:46
 /* RTO calculation flag to say if it
  * is safe to determine local lan or not.
  */
-#define SCTP_DETERMINE_LL_NOTOK 0
-#define SCTP_DETERMINE_LL_OK    1
+#define SCTP_RTT_FROM_NON_DATA 0
+#define SCTP_RTT_FROM_DATA     1
 
 
 /* IP hdr (20/40) + 12+2+2 (enet) + sctp common 12 */
