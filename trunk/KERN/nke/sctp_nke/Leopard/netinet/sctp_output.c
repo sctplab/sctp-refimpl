6424c7d864df8fbc444fe2f107bf2afe6e27d5fc
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-05-17 15:49:20 +0000

    Sync with main sources.

--- trunk/KERN/nke/sctp_nke/Leopard/netinet/sctp_output.c
+++ trunk/KERN/nke/sctp_nke/Leopard/netinet/sctp_output.c
@@ -34,7 +34,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 221904 2011-05-14 18:22:14Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 222028 2011-05-17 15:46:28Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
@@ -2215,33 +2215,33 @@ sctp_is_ifa_addr_preferred(struct sctp_ifa *ifa,
 {
 	uint8_t dest_is_global = 0;
 	/* dest_is_priv is true if destination is a private address */
-        /* dest_is_loop is true if destination is a loopback addresses */
+	/* dest_is_loop is true if destination is a loopback addresses */
 
 	/**
 	 * Here we determine if its a preferred address. A preferred address
 	 * means it is the same scope or higher scope then the destination.
 	 * L = loopback, P = private, G = global
 	 * -----------------------------------------
-         *    src    |  dest | result
-         *  ----------------------------------------
-         *     L     |    L  |    yes
-         *  -----------------------------------------
-         *     P     |    L  |    yes-v4 no-v6
-         *  -----------------------------------------
-         *     G     |    L  |    yes-v4 no-v6
-         *  -----------------------------------------
-         *     L     |    P  |    no
-         *  -----------------------------------------
-         *     P     |    P  |    yes
-         *  -----------------------------------------
-         *     G     |    P  |    no
-         *   -----------------------------------------
-         *     L     |    G  |    no
-         *   -----------------------------------------
-         *     P     |    G  |    no
-         *    -----------------------------------------
-         *     G     |    G  |    yes
-         *    -----------------------------------------
+	 *    src    |  dest | result
+	 *  ----------------------------------------
+	 *     L     |    L  |    yes
+	 *  -----------------------------------------
+	 *     P     |    L  |    yes-v4 no-v6
+	 *  -----------------------------------------
+	 *     G     |    L  |    yes-v4 no-v6
+	 *  -----------------------------------------
+	 *     L     |    P  |    no
+	 *  -----------------------------------------
+	 *     P     |    P  |    yes
+	 *  -----------------------------------------
+	 *     G     |    P  |    no
+	 *   -----------------------------------------
+	 *     L     |    G  |    no
+	 *   -----------------------------------------
+	 *     P     |    G  |    no
+	 *    -----------------------------------------
+	 *     G     |    G  |    yes
+	 *    -----------------------------------------
 	 */
 
 	if (ifa->address.sa.sa_family != fam) {
@@ -2321,14 +2321,14 @@ sctp_is_ifa_addr_acceptable(struct sctp_ifa *ifa,
 	 *
 	 * L = loopback, P = private, G = global
 	 * -----------------------------------------
-         *  src    |  dest | result
+	 *  src    |  dest | result
 	 * -----------------------------------------
 	 *   L     |   L   |    yes
 	 *  -----------------------------------------
 	 *   P     |   L   |    yes-v4 no-v6
 	 *  -----------------------------------------
 	 *   G     |   L   |    yes
-         * -----------------------------------------
+	 * -----------------------------------------
 	 *   L     |   P   |    no
 	 * -----------------------------------------
 	 *   P     |   P   |    yes
@@ -2336,7 +2336,7 @@ sctp_is_ifa_addr_acceptable(struct sctp_ifa *ifa,
 	 *   G     |   P   |    yes - May not work
 	 * -----------------------------------------
 	 *   L     |   G   |    no
-         * -----------------------------------------
+	 * -----------------------------------------
 	 *   P     |   G   |    yes - May not work
 	 * -----------------------------------------
 	 *   G     |   G   |    yes
