a456e2fb680c0d4af7f76491b42c6dfa35909458
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-07-14 11:43:51 +0000

    Whitespace changes.

--- trunk/APPS/debug_tools/assocstat.c
+++ trunk/APPS/debug_tools/assocstat.c
@@ -1,31 +1,31 @@
 /*-
- * Copyright (c) 2009, by Michael Tuexen and Randall Stewart.
- * All rights reserved.
+ * Copyright (c) 2009-2011, by Michael Tuexen. All rights reserved.
+ * Copyright (c) 2009-2011, by Randall Stewart. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
- * a) Redistributions of source code must retain the above copyright notice, 
+ * a) Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- * b) Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
+ * b) Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the distribution.
  *
- * c) Neither the name of the authors nor the names of its 
- *    contributors may be used to endorse or promote products derived 
+ * c) Neither the name of the authors nor the names of its
+ *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -75,7 +75,6 @@ int main(int argc, char *argv[])
 	if (sizeof(stat) != len) {
 		printf("Warning - mis-aligned? retlen:%ld statlen:%ld\n",
 		       (unsigned long)len, (unsigned long)sizeof(stat));
-		       
 	}
 
 #define p(f, n)                                                        \
@@ -296,8 +295,7 @@ int main(int argc, char *argv[])
 			}
 			offset += sizeof(struct xsctp_laddr);
 			printf(".\n");
-			
-			
+
 			xraddr = (struct xsctp_raddr *)(buf + offset);
 			primary_addr = xstcb->primary_addr;
 			while (xraddr->last == 0) {
