3a8a0cbed4869423971bb0b756998692f8b8842d
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-10-27 22:41:18 +0000

    Sync FBSDIDs.

--- trunk/KERN/netinet/sctp_pcb.c
+++ trunk/KERN/netinet/sctp_pcb.c
@@ -34,7 +34,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 226203 2011-10-10 12:28:47Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 226869 2011-10-27 22:38:48Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
