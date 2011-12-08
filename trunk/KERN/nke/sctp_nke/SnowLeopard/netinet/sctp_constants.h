0aa2a7272c728fb42b11aaa96005ab9c8d14b821
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-12-08 14:12:53 +0000

    Sync with main sources.

--- trunk/KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_constants.h
+++ trunk/KERN/nke/sctp_nke/SnowLeopard/netinet/sctp_constants.h
@@ -421,10 +421,12 @@ __FBSDID("$FreeBSD: head/sys/netinet/sctp_constants.h 225635 2011-09-17 08:50:29
 #define SCTP_OUTPUT_FROM_COOKIE_ACK     14
 #define SCTP_OUTPUT_FROM_DRAIN          15
 #define SCTP_OUTPUT_FROM_CLOSING        16
+#define SCTP_OUTPUT_FROM_SOCKOPT        17
+
 /* SCTP chunk types are moved sctp.h for application (NAT, FW) use */
 
 /* align to 32-bit sizes */
-#define SCTP_SIZE32(x)	(uint32_t)((((x)+3) >> 2) << 2)
+#define SCTP_SIZE32(x)	((((x)+3) >> 2) << 2)
 
 #define IS_SCTP_CONTROL(a) ((a)->chunk_type != SCTP_DATA)
 #define IS_SCTP_DATA(a) ((a)->chunk_type == SCTP_DATA)
