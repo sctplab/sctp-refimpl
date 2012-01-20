e870d781aab2b581a21e9b28cb7cab6a78963858
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-01-20 12:30:08 +0000

    Sync with userland code.

--- trunk/KERN/netinet/sctp_os_userspace.h
+++ trunk/KERN/netinet/sctp_os_userspace.h
@@ -1044,6 +1044,14 @@ typedef int SHA384_CTX;
 typedef int SHA512_CTX;
 #endif
 
+#if defined(__Userspace_os_Windows)
+#define SHUT_RD 1
+#define SHUT_WR 2
+#define SHUT_RDWR 3 
+#endif
+#define PRU_FLUSH_RD SHUT_RD
+#define PRU_FLUSH_WR SHUT_WR
+#define PRU_FLUSH_RDWR SHUT_RDWR
 
 /* netinet/ip_var.h defintions are behind an if defined for _KERNEL on FreeBSD */
 #define	IP_RAWOUTPUT		0x2
