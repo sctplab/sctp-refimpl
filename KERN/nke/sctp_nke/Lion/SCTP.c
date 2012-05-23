45b27972bb7e1c67ab5db821f1bd00461b1cb5b3
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-05-23 10:29:26 +0000

    Update Copyright.

--- KERN/nke/sctp_nke/Lion/SCTP.c
+++ KERN/nke/sctp_nke/Lion/SCTP.c
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) 2004-2010 Michael Tuexen, tuexen@fh-muenster.de
- *
- * All rights reserved.
+ * Copyright (c) 2008-2012, by Michael Tuexen. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
