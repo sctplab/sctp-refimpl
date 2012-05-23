88c04d5be3a5e0d3ffd76662f912ac9bfb9ea531
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-05-23 12:03:48 +0000

    Sync with main sources.

--- KERN/nke/sctp_nke/Leopard/netinet/sctp_os_macosx.h
+++ KERN/nke/sctp_nke/Leopard/netinet/sctp_os_macosx.h
@@ -1,7 +1,7 @@
 /*-
  * Copyright (c) 2006-2007, by Cisco Systems, Inc. All rights reserved.
- * Copyright (c) 2008-2011, by Randall Stewart. All rights reserved.
- * Copyright (c) 2008-2011, by Michael Tuexen. All rights reserved.
+ * Copyright (c) 2008-2012, by Randall Stewart. All rights reserved.
+ * Copyright (c) 2008-2012, by Michael Tuexen. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -29,8 +29,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef __sctp_os_macosx_h__
-#define __sctp_os_macosx_h__
+
+#ifndef _NETINET_SCTP_OS_MACOSX_H_
+#define _NETINET_SCTP_OS_MACOSX_H_
 
 /*
  * includes
