623e0d0736aaf24e257f78187fda8fcc7818a5dc
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-05-23 10:42:38 +0000

    Update copyright.

--- trunk/KERN/nke/sctp_nke/Lion/netinet/sctp_macosx.c
+++ trunk/KERN/nke/sctp_nke/Lion/netinet/sctp_macosx.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (C) 2002-2007 Cisco Systems Inc,
- * All rights reserved.
+ * Copyright (c) 2001-2007, by Cisco Systems, Inc. All rights reserved.
+ * Copyright (c) 2005-2012, by Michael Tuexen. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
