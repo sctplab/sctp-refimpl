3ddb611c2036b5a7cfc4dedaf1ccd81d71cada4a
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-07-04 08:10:32 +0000

    Sync FBSDID.

--- trunk/KERN/netinet/sctp_output.c
+++ trunk/KERN/netinet/sctp_output.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 238002 2012-07-02 16:40:11Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 238092 2012-07-04 07:37:53Z glebius $");
 #endif
 
 #include <netinet/sctp_os.h>
