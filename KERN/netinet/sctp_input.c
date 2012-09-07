a9a1163dc11b125286b7b3cda312ef5f42c087f4
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-09-07 13:40:18 +0000

    Sync FBSDIDs.

--- KERN/netinet/sctp_input.c
+++ KERN/netinet/sctp_input.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_input.c 240148 2012-09-05 18:52:01Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_input.c 240198 2012-09-07 13:36:42Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
