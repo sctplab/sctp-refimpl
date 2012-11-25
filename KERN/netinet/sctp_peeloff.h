d4fc127d372303afeec8e85dd85de293fc230df1
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-11-25 14:26:59 +0000

    Sync FBSDIDs.

--- KERN/netinet/sctp_peeloff.h
+++ KERN/netinet/sctp_peeloff.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_peeloff.h 235828 2012-05-23 11:26:28Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_peeloff.h 243516 2012-11-25 14:25:08Z tuexen $");
 #endif
 
 #ifndef _NETINET_SCTP_PEELOFF_H_
