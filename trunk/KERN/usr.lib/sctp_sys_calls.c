e04bb06df7e97d61c77c75a95a4066d4c97436b6
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2013-07-09 19:13:34 +0000

    Sync FBSDID.

--- trunk/KERN/usr.lib/sctp_sys_calls.c
+++ trunk/KERN/usr.lib/sctp_sys_calls.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/lib/libc/net/sctp_sys_calls.c 253104 2013-07-09 19:04:19Z tuexen $");
+__FBSDID("$FreeBSD: head/lib/libc/net/sctp_sys_calls.c 253105 2013-07-09 19:12:47Z tuexen $");
 #endif
 
 #include <stdio.h>
