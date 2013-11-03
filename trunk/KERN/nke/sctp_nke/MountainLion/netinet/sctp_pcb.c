6389382a809ffcd786e7a4aacf48b87cea332ac2
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2013-11-03 14:01:23 +0000

    Sync FBSDID.

--- trunk/KERN/nke/sctp_nke/MountainLion/netinet/sctp_pcb.c
+++ trunk/KERN/nke/sctp_nke/MountainLion/netinet/sctp_pcb.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 257555 2013-11-02 20:12:19Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 257574 2013-11-03 14:00:17Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
