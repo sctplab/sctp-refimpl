63eb2e7ae25906ffce5d9215e863850d648e9240
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-03-12 15:34:00 +0000

    Update FBSDID and sync all files.

--- trunk/KERN/nke/sctp_nke/Mavericks/netinet/sctp.h
+++ trunk/KERN/nke/sctp_nke/Mavericks/netinet/sctp.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 254248 2013-08-12 13:52:15Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp.h 263094 2014-03-12 15:30:40Z tuexen $");
 #endif
 
 #ifndef _NETINET_SCTP_H_
