5224c29490356f4e94fe1873952bcc79d94d9ed9
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-06-19 18:51:33 +0000

    The stcb can be NULL, so better use inp.

--- KERN/nke/sctp_nke/Mavericks/netinet/sctp_output.c
+++ KERN/nke/sctp_nke/Mavericks/netinet/sctp_output.c
@@ -2099,14 +2099,14 @@ sctp_add_addresses_to_i_ia(struct sctp_inpcb *inp, struct sctp_tcb *stcb,
 #if defined(__FreeBSD__)
 #ifdef INET
 				if ((sctp_ifap->address.sa.sa_family == AF_INET) &&
-				    (prison_check_ip4(stcb->sctp_ep->ip_inp.inp.inp_cred,
+				    (prison_check_ip4(inp->ip_inp.inp.inp_cred,
 				                      &sctp_ifap->address.sin.sin_addr) != 0)) {
 				    	continue;
 				}
 #endif
 #ifdef INET6
 				if ((sctp_ifap->address.sa.sa_family == AF_INET6) &&
-				    (prison_check_ip6(stcb->sctp_ep->ip_inp.inp.inp_cred,
+				    (prison_check_ip6(inp->ip_inp.inp.inp_cred,
 				                      &sctp_ifap->address.sin6.sin6_addr) != 0)) {
 				    	continue;
 				}
@@ -2149,14 +2149,14 @@ sctp_add_addresses_to_i_ia(struct sctp_inpcb *inp, struct sctp_tcb *stcb,
 #if defined(__FreeBSD__)
 #ifdef INET
 					if ((sctp_ifap->address.sa.sa_family == AF_INET) &&
-					    (prison_check_ip4(stcb->sctp_ep->ip_inp.inp.inp_cred,
+					    (prison_check_ip4(inp->ip_inp.inp.inp_cred,
 					                      &sctp_ifap->address.sin.sin_addr) != 0)) {
 					    	continue;
 					}
 #endif
 #ifdef INET6
 					if ((sctp_ifap->address.sa.sa_family == AF_INET6) &&
-					    (prison_check_ip6(stcb->sctp_ep->ip_inp.inp.inp_cred,
+					    (prison_check_ip6(inp->ip_inp.inp.inp_cred,
 					                      &sctp_ifap->address.sin6.sin6_addr) != 0)) {
 					    	continue;
 					}
