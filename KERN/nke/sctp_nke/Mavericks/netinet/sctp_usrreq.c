f7d8765f7eb1428c708288c350b2d686b752e4b1
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-06-20 17:47:29 +0000

    Sync FBSDID.

--- KERN/nke/sctp_nke/Mavericks/netinet/sctp_usrreq.c
+++ KERN/nke/sctp_nke/Mavericks/netinet/sctp_usrreq.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_usrreq.c 267674 2014-06-20 13:26:49Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_usrreq.c 267682 2014-06-20 17:45:00Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
