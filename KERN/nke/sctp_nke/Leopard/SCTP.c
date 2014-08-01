692853d472f550639e04c9262cd56853699f48d6
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-08-01 20:56:38 +0000

    Sync with Mavericks and adopt user land stack.

--- KERN/nke/sctp_nke/Leopard/SCTP.c
+++ KERN/nke/sctp_nke/Leopard/SCTP.c
@@ -98,7 +98,6 @@ extern struct sysctl_oid sysctl__net_inet_sctp_cmt_on_off;
 extern struct sysctl_oid sysctl__net_inet_sctp_nr_sack_on_off;
 extern struct sysctl_oid sysctl__net_inet_sctp_cmt_use_dac;
 extern struct sysctl_oid sysctl__net_inet_sctp_cwnd_maxburst;
-extern struct sysctl_oid sysctl__net_inet_sctp_asconf_auth_nochk;
 extern struct sysctl_oid sysctl__net_inet_sctp_auth_disable;
 extern struct sysctl_oid sysctl__net_inet_sctp_nat_friendly;
 extern struct sysctl_oid sysctl__net_inet_sctp_abc_l_var;
@@ -409,7 +408,6 @@ SCTP_start(kmod_info_t * ki __attribute__((unused)), void * d __attribute__((unu
 	sysctl_register_oid(&sysctl__net_inet_sctp_nr_sack_on_off);
 	sysctl_register_oid(&sysctl__net_inet_sctp_cmt_use_dac);
 	sysctl_register_oid(&sysctl__net_inet_sctp_cwnd_maxburst);
-	sysctl_register_oid(&sysctl__net_inet_sctp_asconf_auth_nochk);
 	sysctl_register_oid(&sysctl__net_inet_sctp_auth_disable);
 	sysctl_register_oid(&sysctl__net_inet_sctp_nat_friendly);
 	sysctl_register_oid(&sysctl__net_inet_sctp_abc_l_var);
@@ -562,7 +560,6 @@ SCTP_stop(kmod_info_t * ki __attribute__((unused)), void * d __attribute__((unus
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_nr_sack_on_off);
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_cmt_use_dac);
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_cwnd_maxburst);
-	sysctl_unregister_oid(&sysctl__net_inet_sctp_asconf_auth_nochk);
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_auth_disable);
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_nat_friendly);
 	sysctl_unregister_oid(&sysctl__net_inet_sctp_abc_l_var);
