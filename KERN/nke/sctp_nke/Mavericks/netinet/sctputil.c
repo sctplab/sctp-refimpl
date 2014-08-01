47e3eaebd606d263065c7eeb68f86ac5d6319de1
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-08-01 12:46:45 +0000

    Sync FBSDIDs.

--- KERN/nke/sctp_nke/Mavericks/netinet/sctputil.c
+++ KERN/nke/sctp_nke/Mavericks/netinet/sctputil.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 269075 2014-07-24 18:06:18Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 269376 2014-08-01 12:42:37Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
--- KERN/nke/sctp_nke/Mavericks/netinet/sctputil.h
+++ KERN/nke/sctp_nke/Mavericks/netinet/sctputil.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctputil.h 268537 2014-07-11 21:15:40Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctputil.h 269376 2014-08-01 12:42:37Z tuexen $");
 #endif
 
 #ifndef _NETINET_SCTP_UTIL_H_
