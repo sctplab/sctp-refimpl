0fe60de34b0426b6a3c748a3adf2394bf3ae6c0a
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2014-09-15 20:02:25 +0000

    Sync FBSDID.

--- trunk/KERN/nke/sctp_nke/Mavericks/netinet/sctp_structs.h
+++ trunk/KERN/nke/sctp_nke/Mavericks/netinet/sctp_structs.h
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_structs.h 269945 2014-08-13 15:50:16Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_structs.h 271643 2014-09-15 19:38:34Z tuexen $");
 #endif
 
 #ifndef _NETINET_SCTP_STRUCTS_H_
