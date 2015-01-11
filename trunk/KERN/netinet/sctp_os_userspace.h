59c18953c07390eefad19643347105dfc23a9e20
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2015-01-11 09:49:17 +0000

    Sync file.

--- trunk/KERN/netinet/sctp_os_userspace.h
+++ trunk/KERN/netinet/sctp_os_userspace.h
@@ -218,7 +218,9 @@ typedef char* caddr_t;
 
 #define bzero(buf, len) memset(buf, 0, len)
 #define bcopy(srcKey, dstKey, len) memcpy(dstKey, srcKey, len)
+#if _MSC_VER < 1900
 #define snprintf(data, size, format, ...) _snprintf_s(data, size, _TRUNCATE, format, __VA_ARGS__)
+#endif
 #define inline __inline
 #define __inline__ __inline
 #define	MSG_EOR		0x8		/* data completes record */
@@ -269,11 +271,6 @@ typedef char* caddr_t;
 #else
 #define BYTE_ORDER LITTLE_ENDIAN
 #endif
-
-int Win_getifaddrs(struct ifaddrs**);
-#define getifaddrs(interfaces)  (int)Win_getifaddrs(interfaces)
-int win_if_nametoindex(const char *);
-#define if_nametoindex(x) win_if_nametoindex(x)
 
 #else /* !defined(Userspace_os_Windows) */
 #include <sys/cdefs.h> /* needed? added from old __FreeBSD__ */
@@ -416,6 +413,13 @@ struct ifreq {
 
 #endif
 
+#if defined(__Userspace_os_Windows)
+int Win_getifaddrs(struct ifaddrs**);
+#define getifaddrs(interfaces)  (int)Win_getifaddrs(interfaces)
+int win_if_nametoindex(const char *);
+#define if_nametoindex(x) win_if_nametoindex(x)
+#endif
+
 #define mtx_lock(arg1)
 #define mtx_unlock(arg1)
 #define mtx_assert(arg1,arg2)
@@ -481,12 +485,10 @@ struct sx {int dummy;};
 #include <netinet/in_systm.h>
 #include <netinet/ip.h>
 #endif
-#if defined INET
 #if defined(__Userspace_os_Windows)
 #include <user_ip_icmp.h>
 #else
 #include <netinet/ip_icmp.h>
-#endif
 #endif
 /* #include <netinet/in_pcb.h> ported to userspace */
 #include <user_inpcb.h>
